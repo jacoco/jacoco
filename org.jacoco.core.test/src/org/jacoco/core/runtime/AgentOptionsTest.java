8674a97580324356ab94fe29b4edbd03282054c2
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-06-30 21:03:55 +0000

    Options syntax for the agent.

---
+++ org.jacoco.core.test/src/org/jacoco/core/runtime/AgentOptionsTest.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.runtime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AgentOptions}.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class AgentOptionsTest {
+
+	@Test
+	public void testDefaults() {
+		AgentOptions options = new AgentOptions();
+		assertEquals("jacoco.exec", options.getFile());
+		assertTrue(options.getMerge());
+		assertEquals("", options.toString());
+	}
+
+	@Test
+	public void testGetFile() {
+		AgentOptions options = new AgentOptions("file=/var/test.exec");
+		assertEquals("/var/test.exec", options.getFile());
+	}
+
+	@Test
+	public void testSetFile() {
+		AgentOptions options = new AgentOptions();
+		options.setFile("/var/test.exec");
+		assertEquals("/var/test.exec", options.getFile());
+		assertEquals("file=/var/test.exec", options.toString());
+	}
+
+	@Test
+	public void testGetMergeTrue() {
+		AgentOptions options = new AgentOptions("merge=true");
+		assertTrue(options.getMerge());
+	}
+
+	@Test
+	public void testGetMergeFalse() {
+		AgentOptions options = new AgentOptions("merge=false");
+		assertFalse(options.getMerge());
+	}
+
+	@Test
+	public void testSetMergeTrue() {
+		AgentOptions options = new AgentOptions();
+		options.setMerge(true);
+		assertTrue(options.getMerge());
+		assertEquals("merge=true", options.toString());
+	}
+
+	@Test
+	public void testSetMergeFalse() {
+		AgentOptions options = new AgentOptions();
+		options.setMerge(false);
+		assertFalse(options.getMerge());
+		assertEquals("merge=false", options.toString());
+	}
+
+	@Test
+	public void testToString() {
+		AgentOptions options = new AgentOptions();
+		options.setFile("test.exec");
+		options.setMerge(false);
+		assertEquals("file=test.exec,merge=false", options.toString());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidOptionFormat() {
+		new AgentOptions("file");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidOptionKey() {
+		new AgentOptions("file=test.exec,something=true");
+	}
+
+}
---
+++ org.jacoco.core/src/org/jacoco/core/runtime/AgentOptions.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.runtime;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility to create and parse options for the runtime agent. Options are
+ * represented as a string in the following format:
+ * 
+ * <pre>
+ *   key1=value1,key2=value2,key3=value3
+ * </pre>
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class AgentOptions {
+
+	/**
+	 * Specifies the output file for execution data. Default is
+	 * <code>jacoco.exec</code> in the working directory.
+	 */
+	public static final String FILE = "file";
+
+	/**
+	 * Specifies whether execution data should be appended to the output file.
+	 * Default is <code>true</code>.
+	 */
+	public static final String MERGE = "merge";
+
+	private static final Collection<String> VALID_OPTIONS = Arrays.asList(FILE,
+			MERGE);
+
+	private final Map<String, String> options;
+
+	/**
+	 * New instance with all values set to default.
+	 */
+	public AgentOptions() {
+		this.options = new HashMap<String, String>();
+	}
+
+	/**
+	 * New instance parsed from the given option string.
+	 * 
+	 * @param optionstr
+	 *            string to parse or <code>null</code>
+	 */
+	public AgentOptions(final String optionstr) {
+		this();
+		if (optionstr != null) {
+			for (final String entry : optionstr.split(",")) {
+				final int pos = entry.indexOf('=');
+				if (pos == -1) {
+					throw new IllegalArgumentException(
+							"Invalid agent option syntax.");
+				}
+				final String key = entry.substring(0, pos);
+				if (!VALID_OPTIONS.contains(key)) {
+					throw new IllegalArgumentException("Unknown agent option.");
+				}
+				options.put(key, entry.substring(pos + 1));
+			}
+		}
+	}
+
+	/**
+	 * Returns the output file location.
+	 * 
+	 * @return output file location
+	 */
+	public String getFile() {
+		final String file = options.get(FILE);
+		return file == null ? "jacoco.exec" : file;
+	}
+
+	/**
+	 * Sets the output file location.
+	 * 
+	 * @param file
+	 *            output file location
+	 */
+	public void setFile(final String file) {
+		options.put(FILE, file);
+	}
+
+	/**
+	 * Returns whether the output should be merged with an existing file.
+	 * 
+	 * @return <code>true</code>, when the output should be merged
+	 */
+	public boolean getMerge() {
+		final String merge = options.get(MERGE);
+		return merge == null ? true : Boolean.parseBoolean(merge);
+	}
+
+	/**
+	 * Sets whether the output should be merged with an existing file.
+	 * 
+	 * @param flag
+	 *            <code>true</code>, when the output should be merged
+	 */
+	public void setMerge(final boolean flag) {
+		options.put(MERGE, String.valueOf(flag));
+	}
+
+	/**
+	 * Creates a string representation that can be passed to the agent via the
+	 * command line. Might be the empty string, if no options are set.
+	 */
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		for (final String key : VALID_OPTIONS) {
+			final String value = options.get(key);
+			if (value != null) {
+				if (sb.length() > 0) {
+					sb.append(',');
+				}
+				sb.append(key).append('=').append(value);
+			}
+		}
+		return sb.toString();
+	}
+
+}
