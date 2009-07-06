0948cb944815e6b81bb5e384705c319f78cafbbc
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-07-06 09:15:28 +0000

    Creating a shorter random identifier for agent package names.

---
+++ org.jacoco.build/src/org/jacoco/build/tools/ant/RandomIdTask.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.build.tools.ant;
+
+import java.util.Random;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * This task creates a random identifier and loads it into a property. The
+ * identifier is a valid Java identifier.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class RandomIdTask extends Task {
+
+	private String name;
+
+	/**
+	 * Sets the name of the property where the identifier should be stored.
+	 * 
+	 * @param name
+	 *            property name
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void execute() throws BuildException {
+		final Random random = new Random();
+		while (true) {
+			final int id = random.nextInt();
+			final String idstr = Integer.toString(id, Character.MAX_RADIX);
+			if (Character.isJavaIdentifierStart(idstr.charAt(0))) {
+				getProject().setNewProperty(name, idstr);
+				break;
+			}
+		}
+	}
+
+}
