d8478c420a4497ab95ddcbf459f7dbf35bb8b30c
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-08-05 11:07:16 +0000

    API for JaCoCo version.

---
+++ org.jacoco.core/src/org/jacoco/core/JaCoCo.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core;
+
+/**
+ * Static Meta information about JaCoCo.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class JaCoCo {
+
+	/** Qualified build version of the JaCoCo core library. */
+	public static final String VERSION = "@VERSION@";
+
+}
