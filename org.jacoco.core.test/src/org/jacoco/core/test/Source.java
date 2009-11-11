f069f23fe46683d6a1315d67a38ad04dff386013
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-11-11 21:24:15 +0000

    Integration test infrastructure.

---
+++ org.jacoco.core.test/src/org/jacoco/core/test/Source.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Reads a single source file and allows access to it through special probe
+ * comments in the following format <code>//$line-<i>tag</i>$.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class Source {
+
+	private static final Pattern TAG_PATTERN = Pattern
+			.compile("\\$line-(.*)\\$");
+
+	private final List<String> lines = new ArrayList<String>();
+
+	private final Map<String, Integer> tags = new HashMap<String, Integer>();
+
+	/**
+	 * Reads a source file from the given reader.
+	 * 
+	 * @param reader
+	 * @throws IOException
+	 */
+	public Source(final Reader reader) throws IOException {
+		final BufferedReader buffer = new BufferedReader(reader);
+		for (String l = buffer.readLine(); l != null; l = buffer.readLine()) {
+			addLine(l);
+		}
+	}
+
+	private void addLine(final String l) {
+		lines.add(l);
+		final Matcher m = TAG_PATTERN.matcher(l);
+		if (m.find()) {
+			final String tag = m.group(1);
+			if (tags.put(tag, Integer.valueOf(lines.size())) != null) {
+				throw new IllegalArgumentException("Duplicate tag: " + tag);
+			}
+		}
+	}
+
+	/**
+	 * Returns all lines of the source file as a list.
+	 * 
+	 * @return all lines of the source file
+	 */
+	public List<String> getLines() {
+		return Collections.unmodifiableList(lines);
+	}
+
+	/**
+	 * Returns the line with the given number
+	 * 
+	 * @param nr
+	 *            line number (first line is 1)
+	 * @return line content
+	 */
+	public String getLine(int nr) {
+		return lines.get(nr - 1);
+	}
+
+	/**
+	 * Returns the line number with the given tag
+	 * 
+	 * @param tag
+	 *            tag from a <code>//$line-<i>tag</i>$ marker
+	 * @return line number (first line is 1)
+	 * @throws NoSuchElementException
+	 *             if there is no such tag
+	 */
+	public int getLineNumber(String tag) throws NoSuchElementException {
+		final Integer nr = tags.get(tag);
+		if (nr == null) {
+			throw new NoSuchElementException("Unknown tag: " + tag);
+		}
+		return nr.intValue();
+	}
+
+}
---
+++ org.jacoco.core.test/src/org/jacoco/core/test/SourceTest.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Mountainminds GmbH & Co. KG and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link Source}.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class SourceTest {
+
+	@Test
+	public void testGetLines1() throws IOException {
+		String src = "\na\nbb\n";
+		final Source s = new Source(new StringReader(src));
+		assertEquals(Arrays.asList("", "a", "bb"), s.getLines());
+	}
+
+	@Test
+	public void testGetLines2() throws IOException {
+		String src = "aa\nbb\n;";
+		final Source s = new Source(new StringReader(src));
+		assertEquals(Arrays.asList("aa", "bb", ";"), s.getLines());
+	}
+
+	@Test
+	public void testGetLines3() throws IOException {
+		String src = "xx\r\nyy";
+		final Source s = new Source(new StringReader(src));
+		assertEquals(Arrays.asList("xx", "yy"), s.getLines());
+	}
+
+	@Test
+	public void testGetLine() throws IOException {
+		String src = "Hello\n\nWorld!";
+		final Source s = new Source(new StringReader(src));
+		assertEquals("Hello", s.getLine(1));
+		assertEquals("", s.getLine(2));
+		assertEquals("World!", s.getLine(3));
+	}
+
+	@Test
+	public void testGetLineNumber() throws IOException {
+		String src = "a\nb$line-tag$\nc\nd\ne$line-tagx$\nf";
+		final Source s = new Source(new StringReader(src));
+		assertEquals(2, s.getLineNumber("tag"), 0.0);
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testGetLineNumberNegative() throws IOException {
+		String src = "a\nb$line-tag$\nc\nd\ne\nf";
+		final Source s = new Source(new StringReader(src));
+		s.getLineNumber("ag");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDuplicateTag() throws IOException {
+		String src = "a\nb$line-tag$\nc\nd\ne$line-tag$\nf";
+		new Source(new StringReader(src));
+	}
+
+}
