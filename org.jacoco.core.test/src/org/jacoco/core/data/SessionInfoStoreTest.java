8de322c23a5b05cc1a21527f8929507142fdb860
Marc R. Hoffmann <hoffmann@mountainminds.com>
2010-05-04 16:23:00 +0000

    Trac #88: Storage facility for SessionInfo objects.

---
+++ org.jacoco.core.test/src/org/jacoco/core/data/SessionInfoStoreTest.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link SessionInfoStore}.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class SessionInfoStoreTest {
+
+	private SessionInfoStore store;
+
+	@Before
+	public void setup() {
+		store = new SessionInfoStore();
+	}
+
+	@Test
+	public void testEmpty() {
+		assertEquals(Collections.emptyList(), store.getInfos());
+		assertTrue(store.isEmpty());
+	}
+
+	@Test
+	public void testIsEmpty() {
+		store.visitSessionInfo(new SessionInfo("A", 123, 456));
+		assertFalse(store.isEmpty());
+	}
+
+	@Test
+	public void testGetInfos() {
+		final SessionInfo a = new SessionInfo("A", 12345, 500000);
+		store.visitSessionInfo(a);
+		final SessionInfo b = new SessionInfo("B", 12345, 400000);
+		store.visitSessionInfo(b);
+		final SessionInfo c = new SessionInfo("C", 12345, 600000);
+		store.visitSessionInfo(c);
+		assertEquals(Arrays.asList(b, a, c), store.getInfos());
+	}
+
+	@Test
+	public void testGetMergedEmpty() {
+		final SessionInfo info = store.getMerged("MERGE");
+		assertEquals("MERGE", info.getId());
+		assertEquals(0, info.getStartTimeStamp());
+		assertEquals(0, info.getDumpTimeStamp());
+	}
+
+	@Test
+	public void testGetMerged() {
+		store.visitSessionInfo(new SessionInfo("A", 23456, 500000));
+		store.visitSessionInfo(new SessionInfo("B", 12345, 400000));
+		store.visitSessionInfo(new SessionInfo("C", 34567, 600000));
+		final SessionInfo info = store.getMerged("MERGE");
+		assertEquals("MERGE", info.getId());
+		assertEquals(12345, info.getStartTimeStamp());
+		assertEquals(600000, info.getDumpTimeStamp());
+	}
+
+	@Test
+	public void testAccept() {
+		final SessionInfo a = new SessionInfo("A", 12345, 500000);
+		store.visitSessionInfo(a);
+		final SessionInfo b = new SessionInfo("B", 12345, 400000);
+		store.visitSessionInfo(b);
+		final SessionInfo c = new SessionInfo("C", 12345, 600000);
+		store.visitSessionInfo(c);
+		final List<SessionInfo> actual = new ArrayList<SessionInfo>();
+		store.accept(new ISessionInfoVisitor() {
+			public void visitSessionInfo(SessionInfo info) {
+				actual.add(info);
+			}
+		});
+		assertEquals(Arrays.asList(b, a, c), actual);
+	}
+
+}
---
+++ org.jacoco.core/src/org/jacoco/core/data/SessionInfoStore.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ * $Id: $
+ *******************************************************************************/
+package org.jacoco.core.data;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Container to collect and merge session {@link SessionInfo} objects. A
+ * instance of this class is not thread safe.
+ * 
+ * @author Marc R. Hoffmann
+ * @version $Revision: $
+ */
+public class SessionInfoStore implements ISessionInfoVisitor {
+
+	private final List<SessionInfo> infos = new ArrayList<SessionInfo>();
+
+	/**
+	 * Tests whether the store is empty.
+	 * 
+	 * @return <code>true</code> if the store is empty
+	 */
+	public boolean isEmpty() {
+		return infos.isEmpty();
+	}
+
+	/**
+	 * Returns all {@link SessionInfo} objects currently contained in the store.
+	 * The info objects are ordered by its natural ordering (i.e. by the dump
+	 * time stamp).
+	 * 
+	 * @return list of stored {@link SessionInfo} objects
+	 */
+	public List<SessionInfo> getInfos() {
+		final List<SessionInfo> copy = new ArrayList<SessionInfo>(infos);
+		Collections.sort(copy);
+		return copy;
+	}
+
+	/**
+	 * Returns a new session info with the given id that contains a merged
+	 * version from all contained version. The start timestamp is the minimum of
+	 * all contained sessions, the dump timestamp the maximum of all contained
+	 * sessions. If no session is currently contained both timestamps are set to
+	 * <code>0</code>.
+	 * 
+	 * @param id
+	 *            identifier for the merged session info
+	 * @return new {@link SessionInfo} object
+	 * 
+	 */
+	public SessionInfo getMerged(final String id) {
+		if (infos.isEmpty()) {
+			return new SessionInfo(id, 0, 0);
+		}
+		long start = Long.MAX_VALUE;
+		long dump = Long.MIN_VALUE;
+		for (final SessionInfo i : infos) {
+			start = min(start, i.getStartTimeStamp());
+			dump = max(dump, i.getDumpTimeStamp());
+		}
+		return new SessionInfo(id, start, dump);
+	}
+
+	/**
+	 * Writes all contained {@link SessionInfo} objects into the given visitor.
+	 * The info objects are emitted in chronological order by dump timestamp.
+	 * 
+	 * @param visitor
+	 *            visitor to emit {@link SessionInfo} objects to
+	 */
+	public void accept(final ISessionInfoVisitor visitor) {
+		for (final SessionInfo i : getInfos()) {
+			visitor.visitSessionInfo(i);
+		}
+	}
+
+	// === ISessionInfoVisitor ===
+
+	public void visitSessionInfo(final SessionInfo info) {
+		infos.add(info);
+	}
+
+}
