ecf745056484a44f72f2960f2b004b4abb7abf4b
Marc R. Hoffmann <hoffmann@mountainminds.com>
2010-12-11 02:54:22 +0000

    Track #66: document some considerations

--- org.jacoco.doc/diagrams/render.sh
+++ org.jacoco.doc/diagrams/render.sh
@@ -2,5 +2,11 @@
 
 OUTPUT=../docroot/doc/.resources
 
-dot -Tpng -o$OUTPUT/flow-1.png flow-1.dot
-dot -Tpng -o$OUTPUT/flow-2.png flow-2.dot
+dot -Tpng -o$OUTPUT/flow-1.png  flow-1.dot
+dot -Tpng -o$OUTPUT/flow-2.png  flow-2.dot
+dot -Tpng -o$OUTPUT/flow-3a.png flow-3a.dot
+dot -Tpng -o$OUTPUT/flow-3b.png flow-3b.dot
+dot -Tpng -o$OUTPUT/flow-4a.png flow-4a.dot
+dot -Tpng -o$OUTPUT/flow-4b.png flow-4b.dot
+dot -Tpng -o$OUTPUT/flow-5a.png flow-5a.dot
+dot -Tpng -o$OUTPUT/flow-5b.png flow-5b.dot
