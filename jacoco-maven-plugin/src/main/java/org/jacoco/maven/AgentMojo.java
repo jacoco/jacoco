1ed47f176f7fdef9b892e05d1f6e72a4f0bf2790
Evgeny Mandrikov <mandrikov@gmail.com>
2011-06-19 18:05:28 +0000

    Reformat code of jacoco-maven-plugin.

--- jacoco-maven-plugin/src/main/java/org/jacoco/maven/AgentMojo.java
+++ jacoco-maven-plugin/src/main/java/org/jacoco/maven/AgentMojo.java
@@ -34,201 +34,219 @@ import java.util.Properties;
  */
 public class AgentMojo extends AbstractMojo {
 
-  /**
-   * Name of the JaCoCo Agent artifact.
-   */
-  private static final String AGENT_ARTIFACT_NAME = "org.jacoco:org.jacoco.agent.rt";
+	/**
+	 * Name of the JaCoCo Agent artifact.
+	 */
+	private static final String AGENT_ARTIFACT_NAME = "org.jacoco:org.jacoco.agent.rt";
 
-  /**
-   * Name of the property used in maven-osgi-test-plugin.
-   */
-  private static final String TYCHO_ARG_LINE = "tycho.testArgLine";
+	/**
+	 * Name of the property used in maven-osgi-test-plugin.
+	 */
+	private static final String TYCHO_ARG_LINE = "tycho.testArgLine";
 
-  /**
-   * Name of the property used in maven-surefire-plugin.
-   */
-  private static final String SUREFIRE_ARG_LINE = "argLine";
+	/**
+	 * Name of the property used in maven-surefire-plugin.
+	 */
+	private static final String SUREFIRE_ARG_LINE = "argLine";
 
-  /**
-   * Maven project.
-   * 
-   * @parameter expression="${project}"
-   * @readonly
-   */
-  private MavenProject project;
+	/**
+	 * Maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	private MavenProject project;
 
-  /**
-   * Map of plugin artifacts.
-   *
-   * @parameter expression="${plugin.artifactMap}"
-   * @required
-   * @readonly
-   */
-  private Map pluginArtifactMap;
+	/**
+	 * Map of plugin artifacts.
+	 * 
+	 * @parameter expression="${plugin.artifactMap}"
+	 * @required
+	 * @readonly
+	 */
+	private Map pluginArtifactMap;
 
-  /**
-   * Allows to specify property which will contains settings for JaCoCo Agent.
-   * If not specified, then "argLine" would be used for "jar" packaging and "tycho.testArgLine" for "eclipse-test-plugin".
-   * 
-   * @parameter
-   */
-  private String propertyName;
+	/**
+	 * Allows to specify property which will contains settings for JaCoCo Agent.
+	 * If not specified, then "argLine" would be used for "jar" packaging and
+	 * "tycho.testArgLine" for "eclipse-test-plugin".
+	 * 
+	 * @parameter
+	 */
+	private String propertyName;
 
-  /**
-   * Path to the output file for execution data.
-   * 
-   * @parameter default-value="${project.build.directory}/jacoco.exec"
-   */
-  private File destfile;
+	/**
+	 * Path to the output file for execution data.
+	 * 
+	 * @parameter default-value="${project.build.directory}/jacoco.exec"
+	 */
+	private File destfile;
 
-  /**
-   * If set to true and the execution data file already exists, coverage data is appended to the existing file.
-   * If set to false, an existing execution data file will be replaced.
-   * 
-   * @parameter default-value="true"
-   */
-  private boolean append;
+	/**
+	 * If set to true and the execution data file already exists, coverage data
+	 * is appended to the existing file. If set to false, an existing execution
+	 * data file will be replaced.
+	 * 
+	 * @parameter default-value="true"
+	 */
+	private boolean append;
 
-  /**
-   * A list of class names that should be included in execution analysis.
-   * The list entries are separated by a colon (:) and may use wildcard characters (* and ?).
-   * Except for performance optimization or technical corner cases this option is normally not required.
-   * 
-   * @parameter default-value="*"
-   */
-  private String includes;
+	/**
+	 * A list of class names that should be included in execution analysis. The
+	 * list entries are separated by a colon (:) and may use wildcard characters
+	 * (* and ?). Except for performance optimization or technical corner cases
+	 * this option is normally not required.
+	 * 
+	 * @parameter default-value="*"
+	 */
+	private String includes;
 
-  /**
-   * A list of class names that should be excluded from execution analysis.
-   * 
-   * The list entries are separated by a colon (:) and may use wildcard characters (* and ?).
-   * Except for performance optimization or technical corner cases this option is normally not required.
-   * 
-   * @parameter default-value=""
-   */
-  private String excludes;
+	/**
+	 * A list of class names that should be excluded from execution analysis.
+	 * 
+	 * The list entries are separated by a colon (:) and may use wildcard
+	 * characters (* and ?). Except for performance optimization or technical
+	 * corner cases this option is normally not required.
+	 * 
+	 * @parameter default-value=""
+	 */
+	private String excludes;
 
-  /**
-   * A list of class loader names, that should be excluded from execution analysis.
-   * The list entries are separated by a colon (:) and may use wildcard characters (* and ?).
-   * This option might be required in case of special frameworks that conflict with JaCoCo code instrumentation,
-   * in particular class loaders that do not have access to the Java runtime classes.
-   * 
-   * @parameter @default-value="sun.reflect.DelegatingClassLoader"
-   */
-  private String exclclassloaders;
+	/**
+	 * A list of class loader names, that should be excluded from execution
+	 * analysis. The list entries are separated by a colon (:) and may use
+	 * wildcard characters (* and ?). This option might be required in case of
+	 * special frameworks that conflict with JaCoCo code instrumentation, in
+	 * particular class loaders that do not have access to the Java runtime
+	 * classes.
+	 * 
+	 * @parameter @default-value="sun.reflect.DelegatingClassLoader"
+	 */
+	private String exclclassloaders;
 
-  /**
-   * A session identifier that is written with the execution data. Without this parameter a random identifier is created by the agent.
-   * 
-   * @parameter
-   */
-  private String sessionid;
+	/**
+	 * A session identifier that is written with the execution data. Without
+	 * this parameter a random identifier is created by the agent.
+	 * 
+	 * @parameter
+	 */
+	private String sessionid;
 
-  /**
-   * If set to true coverage data will be written on VM shutdown.
-   * 
-   * @parameter default-value="true"
-   */
-  private boolean dumpOnExit;
+	/**
+	 * If set to true coverage data will be written on VM shutdown.
+	 * 
+	 * @parameter default-value="true"
+	 */
+	private boolean dumpOnExit;
 
-  /**
-   * Output method to use for writing coverage data. Valid options are:
-   * <ul>
-   * <li>file: At VM termination execution data is written to the file specified in the tofile attribute.</li>
-   * <li>tcpserver: The agent listens for incoming connections on the TCP port specified by the address and port attribute. Execution data is written to this TCP connection.</li>
-   * <li>tcpclient: At startup the agent connects to the TCP port specified by the address and port attribute. Execution data is written to this TCP connection.</li>
-   * </ul>
-   * 
-   * @parameter default-value="file"
-   */
-  private String output;
+	/**
+	 * Output method to use for writing coverage data. Valid options are:
+	 * <ul>
+	 * <li>file: At VM termination execution data is written to the file
+	 * specified in the tofile attribute.</li>
+	 * <li>tcpserver: The agent listens for incoming connections on the TCP port
+	 * specified by the address and port attribute. Execution data is written to
+	 * this TCP connection.</li>
+	 * <li>tcpclient: At startup the agent connects to the TCP port specified by
+	 * the address and port attribute. Execution data is written to this TCP
+	 * connection.</li>
+	 * </ul>
+	 * 
+	 * @parameter default-value="file"
+	 */
+	private String output;
 
-  /**
-   * IP address or hostname to bind to when the output method is tcpserver or connect to when the output method is tcpclient.
-   * In tcpserver mode the value "*" causes the agent to accept connections on any local address.
-   * 
-   * @parameter
-   */
-  private String address;
+	/**
+	 * IP address or hostname to bind to when the output method is tcpserver or
+	 * connect to when the output method is tcpclient. In tcpserver mode the
+	 * value "*" causes the agent to accept connections on any local address.
+	 * 
+	 * @parameter
+	 */
+	private String address;
 
-  /**
-   * Port to bind to when the output method is tcpserver or connect to when the output method is tcpclient.
-   * In tcpserver mode the port must be available, which means that if multiple JaCoCo agents should run on the same machine, different ports have to be specified.
-   * 
-   * @parameter default-value="6300"
-   */
-  private int port;
+	/**
+	 * Port to bind to when the output method is tcpserver or connect to when
+	 * the output method is tcpclient. In tcpserver mode the port must be
+	 * available, which means that if multiple JaCoCo agents should run on the
+	 * same machine, different ports have to be specified.
+	 * 
+	 * @parameter default-value="6300"
+	 */
+	private int port;
 
-  public void execute() throws MojoExecutionException, MojoFailureException {
-    if (!isEclipseTestPluginPackaging() && !isJarPackaging() && !isPropertyNameSpecified()) {
-      return;
-    }
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		if (!isEclipseTestPluginPackaging() && !isJarPackaging()
+				&& !isPropertyNameSpecified()) {
+			return;
+		}
 
-    String vmArgument = createAgentOptions().getVMArgument(getAgentJarFile());
-    if (isPropertyNameSpecified()) {
-      prependProperty(propertyName, vmArgument);
-    } else if (isEclipseTestPluginPackaging()) {
-      prependProperty(TYCHO_ARG_LINE, vmArgument);
-    } else {
-      prependProperty(SUREFIRE_ARG_LINE, vmArgument);
-    }
-  }
+		String vmArgument = createAgentOptions().getVMArgument(
+				getAgentJarFile());
+		if (isPropertyNameSpecified()) {
+			prependProperty(propertyName, vmArgument);
+		} else if (isEclipseTestPluginPackaging()) {
+			prependProperty(TYCHO_ARG_LINE, vmArgument);
+		} else {
+			prependProperty(SUREFIRE_ARG_LINE, vmArgument);
+		}
+	}
 
-  private File getAgentJarFile() throws MojoExecutionException {
-    Artifact jacocoAgentArtifact = (Artifact) pluginArtifactMap.get(AGENT_ARTIFACT_NAME);
-    return jacocoAgentArtifact.getFile();
-  }
+	private File getAgentJarFile() throws MojoExecutionException {
+		Artifact jacocoAgentArtifact = (Artifact) pluginArtifactMap
+				.get(AGENT_ARTIFACT_NAME);
+		return jacocoAgentArtifact.getFile();
+	}
 
-  private AgentOptions createAgentOptions() throws MojoExecutionException {
-    AgentOptions agentOptions = new AgentOptions();
-    String destPath = destfile.getAbsolutePath();
-    try {
-      destPath = CommandLineUtils.quote(destPath);
-    } catch (CommandLineException e) {
-      throw new MojoExecutionException("Failure to use path", e);
-    }
-    agentOptions.setDestfile(destPath);
-    agentOptions.setAppend(append);
-    if (includes != null) {
-      agentOptions.setIncludes(includes);
-    }
-    if (excludes != null) {
-      agentOptions.setExcludes(excludes);
-    }
-    if (exclclassloaders != null) {
-      agentOptions.setExclClassloader(exclclassloaders);
-    }
-    if (sessionid != null) {
-      agentOptions.setSessionId(sessionid);
-    }
-    agentOptions.setDumpOnExit(dumpOnExit);
-    agentOptions.setOutput(output);
-    if (address != null) {
-      agentOptions.setAddress(address);
-    }
-    agentOptions.setPort(port);
-    return agentOptions;
-  }
+	private AgentOptions createAgentOptions() throws MojoExecutionException {
+		AgentOptions agentOptions = new AgentOptions();
+		String destPath = destfile.getAbsolutePath();
+		try {
+			destPath = CommandLineUtils.quote(destPath);
+		} catch (CommandLineException e) {
+			throw new MojoExecutionException("Failure to use path", e);
+		}
+		agentOptions.setDestfile(destPath);
+		agentOptions.setAppend(append);
+		if (includes != null) {
+			agentOptions.setIncludes(includes);
+		}
+		if (excludes != null) {
+			agentOptions.setExcludes(excludes);
+		}
+		if (exclclassloaders != null) {
+			agentOptions.setExclClassloader(exclclassloaders);
+		}
+		if (sessionid != null) {
+			agentOptions.setSessionId(sessionid);
+		}
+		agentOptions.setDumpOnExit(dumpOnExit);
+		agentOptions.setOutput(output);
+		if (address != null) {
+			agentOptions.setAddress(address);
+		}
+		agentOptions.setPort(port);
+		return agentOptions;
+	}
 
-  private boolean isPropertyNameSpecified() {
-    return propertyName != null && !"".equals(propertyName);
-  }
+	private boolean isPropertyNameSpecified() {
+		return propertyName != null && !"".equals(propertyName);
+	}
 
-  private boolean isEclipseTestPluginPackaging() {
-    return "eclipse-test-plugin".equals(project.getPackaging());
-  }
+	private boolean isEclipseTestPluginPackaging() {
+		return "eclipse-test-plugin".equals(project.getPackaging());
+	}
 
-  private boolean isJarPackaging() {
-    return "jar".equals(project.getPackaging());
-  }
+	private boolean isJarPackaging() {
+		return "jar".equals(project.getPackaging());
+	}
 
-  private void prependProperty(String name, String value) {
-    Properties projectProperties = project.getProperties();
-    String oldValue = projectProperties.getProperty(name);
-    String newValue = oldValue == null ? value : value + ' ' + oldValue;
-    getLog().info(name + " set to " + value);
-    projectProperties.put(name, newValue);
-  }
+	private void prependProperty(String name, String value) {
+		Properties projectProperties = project.getProperties();
+		String oldValue = projectProperties.getProperty(name);
+		String newValue = oldValue == null ? value : value + ' ' + oldValue;
+		getLog().info(name + " set to " + value);
+		projectProperties.put(name, newValue);
+	}
 
 }
--- jacoco-maven-plugin/src/main/java/org/jacoco/maven/ReportMojo.java
+++ jacoco-maven-plugin/src/main/java/org/jacoco/maven/ReportMojo.java
@@ -39,168 +39,182 @@ import java.util.List;
  */
 public class ReportMojo extends AbstractMojo {
 
-  /**
-   * Maven project.
-   * 
-   * @parameter expression="${project}"
-   * @readonly
-   */
-  private MavenProject project;
+	/**
+	 * Maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	private MavenProject project;
 
-  /**
-   * Output directory for the reports.
-   * 
-   * @parameter default-value="${project.reporting.outputDirectory}/jacoco"
-   */
-  private File outputDirectory;
+	/**
+	 * Output directory for the reports.
+	 * 
+	 * @parameter default-value="${project.reporting.outputDirectory}/jacoco"
+	 */
+	private File outputDirectory;
 
-  /**
-   * Encoding of the generated reports.
-   * 
-   * @parameter expression="${project.reporting.outputEncoding}" default-value="UTF-8" 
-   */
-  private String outputEncoding;
+	/**
+	 * Encoding of the generated reports.
+	 * 
+	 * @parameter expression="${project.reporting.outputEncoding}"
+	 *            default-value="UTF-8"
+	 */
+	private String outputEncoding;
 
-  /**
-   * Encoding of the source files.
-   * 
-   * @parameter expression="${project.build.sourceEncoding}" default-value="UTF-8"
-   */
-  private String sourceEncoding;
+	/**
+	 * Encoding of the source files.
+	 * 
+	 * @parameter expression="${project.build.sourceEncoding}"
+	 *            default-value="UTF-8"
+	 */
+	private String sourceEncoding;
 
-  /**
-   * File with execution data.
-   * 
-   * @parameter default-value="${project.build.directory}/jacoco.exec"
-   */
-  private File dataFile;
+	/**
+	 * File with execution data.
+	 * 
+	 * @parameter default-value="${project.build.directory}/jacoco.exec"
+	 */
+	private File dataFile;
 
-  private SessionInfoStore sessionInfoStore;
+	private SessionInfoStore sessionInfoStore;
 
-  private ExecutionDataStore executionDataStore;
+	private ExecutionDataStore executionDataStore;
 
-  public void execute() throws MojoExecutionException, MojoFailureException {
-    loadExecutionData();
-    try {
-      IReportVisitor visitor = createVisitor();
-      visitor.visitInfo(sessionInfoStore.getInfos(), executionDataStore.getContents());
-      createReport(visitor);
-      visitor.visitEnd();
-    } catch (Exception e) {
-      throw new MojoExecutionException("Error while creating report", e);
-    }
-  }
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		loadExecutionData();
+		try {
+			IReportVisitor visitor = createVisitor();
+			visitor.visitInfo(sessionInfoStore.getInfos(),
+					executionDataStore.getContents());
+			createReport(visitor);
+			visitor.visitEnd();
+		} catch (Exception e) {
+			throw new MojoExecutionException("Error while creating report", e);
+		}
+	}
 
-  private void loadExecutionData() throws MojoExecutionException {
-    sessionInfoStore = new SessionInfoStore();
-    executionDataStore = new ExecutionDataStore();
-    FileInputStream in = null;
-    try {
-      in = new FileInputStream(dataFile);
-      ExecutionDataReader reader = new ExecutionDataReader(in);
-      reader.setSessionInfoVisitor(sessionInfoStore);
-      reader.setExecutionDataVisitor(executionDataStore);
-      reader.read();
-    } catch (IOException e) {
-      throw new MojoExecutionException("Unable to read execution data file " + dataFile, e);
-    } finally {
-      if (in != null) {
-        try {
-          in.close();
-        } catch (IOException e) {
-        }
-      }
-    }
-  }
+	private void loadExecutionData() throws MojoExecutionException {
+		sessionInfoStore = new SessionInfoStore();
+		executionDataStore = new ExecutionDataStore();
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(dataFile);
+			ExecutionDataReader reader = new ExecutionDataReader(in);
+			reader.setSessionInfoVisitor(sessionInfoStore);
+			reader.setExecutionDataVisitor(executionDataStore);
+			reader.read();
+		} catch (IOException e) {
+			throw new MojoExecutionException(
+					"Unable to read execution data file " + dataFile, e);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
 
-  private void createReport(final IReportGroupVisitor visitor) throws IOException {
-    final IBundleCoverage bundle = createBundle();
-    final SourceFileCollection locator = new SourceFileCollection(getCompileSourceRoots(), sourceEncoding);
-    checkForMissingDebugInformation(bundle);
-    visitor.visitBundle(bundle, locator);
-  }
+	private void createReport(final IReportGroupVisitor visitor)
+			throws IOException {
+		final IBundleCoverage bundle = createBundle();
+		final SourceFileCollection locator = new SourceFileCollection(
+				getCompileSourceRoots(), sourceEncoding);
+		checkForMissingDebugInformation(bundle);
+		visitor.visitBundle(bundle, locator);
+	}
 
-  private void checkForMissingDebugInformation(ICoverageNode node) {
-    if (node.getClassCounter().getTotalCount() > 0 && node.getLineCounter().getTotalCount() == 0) {
-      getLog().warn("To enable source code annotation class files have to be compiled with debug information.");
-    }
-  }
+	private void checkForMissingDebugInformation(ICoverageNode node) {
+		if (node.getClassCounter().getTotalCount() > 0
+				&& node.getLineCounter().getTotalCount() == 0) {
+			getLog().warn(
+					"To enable source code annotation class files have to be compiled with debug information.");
+		}
+	}
 
-  private IBundleCoverage createBundle() throws IOException {
-    final CoverageBuilder builder = new CoverageBuilder();
-    final Analyzer analyzer = new Analyzer(executionDataStore, builder);
-    File classesDir = new File(project.getBuild().getOutputDirectory());
-    analyzer.analyzeAll(classesDir);
-    return builder.getBundle(project.getName());
-  }
+	private IBundleCoverage createBundle() throws IOException {
+		final CoverageBuilder builder = new CoverageBuilder();
+		final Analyzer analyzer = new Analyzer(executionDataStore, builder);
+		File classesDir = new File(project.getBuild().getOutputDirectory());
+		analyzer.analyzeAll(classesDir);
+		return builder.getBundle(project.getName());
+	}
 
-  private IReportVisitor createVisitor() throws IOException {
-    List<IReportVisitor> visitors = new ArrayList<IReportVisitor>();
+	private IReportVisitor createVisitor() throws IOException {
+		List<IReportVisitor> visitors = new ArrayList<IReportVisitor>();
 
-    outputDirectory.mkdirs();
+		outputDirectory.mkdirs();
 
-    final XMLFormatter xmlFormatter = new XMLFormatter();
-    xmlFormatter.setOutputEncoding(outputEncoding);
-    visitors.add(xmlFormatter.createVisitor(new FileOutputStream(new File(outputDirectory, "jacoco.xml"))));
+		final XMLFormatter xmlFormatter = new XMLFormatter();
+		xmlFormatter.setOutputEncoding(outputEncoding);
+		visitors.add(xmlFormatter.createVisitor(new FileOutputStream(new File(
+				outputDirectory, "jacoco.xml"))));
 
-    final CSVFormatter formatter = new CSVFormatter();
-    formatter.setOutputEncoding(outputEncoding);
-    visitors.add(formatter.createVisitor(new FileOutputStream(new File(outputDirectory, "jacoco.csv"))));
+		final CSVFormatter formatter = new CSVFormatter();
+		formatter.setOutputEncoding(outputEncoding);
+		visitors.add(formatter.createVisitor(new FileOutputStream(new File(
+				outputDirectory, "jacoco.csv"))));
 
-    final HTMLFormatter htmlFormatter = new HTMLFormatter();
-    // formatter.setFooterText(footer);
-    htmlFormatter.setOutputEncoding(outputEncoding);
-    // formatter.setLocale(locale);
-    visitors.add(htmlFormatter.createVisitor(new FileMultiReportOutput(outputDirectory)));
+		final HTMLFormatter htmlFormatter = new HTMLFormatter();
+		// formatter.setFooterText(footer);
+		htmlFormatter.setOutputEncoding(outputEncoding);
+		// formatter.setLocale(locale);
+		visitors.add(htmlFormatter.createVisitor(new FileMultiReportOutput(
+				outputDirectory)));
 
-    return new MultiReportVisitor(visitors);
-  }
+		return new MultiReportVisitor(visitors);
+	}
 
-  private static class SourceFileCollection implements ISourceFileLocator {
+	private static class SourceFileCollection implements ISourceFileLocator {
 
-    private List<File> sourceRoots;
-    private String encoding;
+		private List<File> sourceRoots;
+		private String encoding;
 
-    public SourceFileCollection(List<File> sourceRoots, String encoding) {
-      this.sourceRoots = sourceRoots;
-      this.encoding = encoding;
-    }
+		public SourceFileCollection(List<File> sourceRoots, String encoding) {
+			this.sourceRoots = sourceRoots;
+			this.encoding = encoding;
+		}
 
-    public Reader getSourceFile(String packageName, String fileName) throws IOException {
-      final String r;
-      if (packageName.length() > 0) {
-        r = packageName + '/' + fileName;
-      } else {
-        r = fileName;
-      }
-      for (File sourceRoot : sourceRoots) {
-        File file = new File(sourceRoot, r);
-        if (file.exists() && file.isFile()) {
-          return new InputStreamReader(new FileInputStream(file), encoding);
-        }
-      }
-      return null;
-    }
+		public Reader getSourceFile(String packageName, String fileName)
+				throws IOException {
+			final String r;
+			if (packageName.length() > 0) {
+				r = packageName + '/' + fileName;
+			} else {
+				r = fileName;
+			}
+			for (File sourceRoot : sourceRoots) {
+				File file = new File(sourceRoot, r);
+				if (file.exists() && file.isFile()) {
+					return new InputStreamReader(new FileInputStream(file),
+							encoding);
+				}
+			}
+			return null;
+		}
 
-    public int getTabWidth() {
-      return 4;
-    }
-  }
+		public int getTabWidth() {
+			return 4;
+		}
+	}
 
-  private File resolvePath(String path) {
-    File file = new File(path);
-    if (!file.isAbsolute()) {
-      file = new File(project.getBasedir(), path);
-    }
-    return file;
-  }
+	private File resolvePath(String path) {
+		File file = new File(path);
+		if (!file.isAbsolute()) {
+			file = new File(project.getBasedir(), path);
+		}
+		return file;
+	}
 
-  private List<File> getCompileSourceRoots() {
-    List<File> result = new ArrayList<File>();
-    for (Object path : project.getExecutionProject().getCompileSourceRoots()) {
-      result.add(resolvePath((String) path));
-    }
-    return result;
-  }
+	private List<File> getCompileSourceRoots() {
+		List<File> result = new ArrayList<File>();
+		for (Object path : project.getExecutionProject()
+				.getCompileSourceRoots()) {
+			result.add(resolvePath((String) path));
+		}
+		return result;
+	}
 
 }
