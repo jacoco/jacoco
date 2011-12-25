8b73c3c455431e8f5400595dcb193b1427cbce48
Evgeny Mandrikov <mandrikov@gmail.com>
2011-12-25 18:44:09 +0000

    Trac #181: Allow to use jacoco-maven-plugin together with maven-site-plugin

--- jacoco-maven-plugin/src/org/jacoco/maven/ReportMojo.java
+++ jacoco-maven-plugin/src/org/jacoco/maven/ReportMojo.java
@@ -14,7 +14,13 @@ package org.jacoco.maven;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.StringUtils;
 import org.jacoco.core.analysis.Analyzer;
@@ -36,10 +42,14 @@ import org.jacoco.report.xml.XMLFormatter;
  * @goal report
  * @requiresProject true
  */
-public class ReportMojo extends AbstractJacocoMojo {
+public class ReportMojo extends AbstractMavenReport {
 
 	/**
-	 * Output directory for the reports.
+	 * Output directory for the reports. Note that this parameter is only
+	 * relevant if the goal is run from the command line or from the default
+	 * build lifecycle. If the goal is run indirectly as part of a site
+	 * generation, the output directory configured in the Maven Site Plugin is
+	 * used instead.
 	 * 
 	 * @parameter default-value="${project.reporting.outputDirectory}/jacoco"
 	 */
@@ -68,12 +78,132 @@ public class ReportMojo extends AbstractJacocoMojo {
 	 */
 	private File dataFile;
 
+	/**
+	 * A list of class files to include in instrumentation/analysis/reports. May
+	 * use wildcard characters (* and ?). When not specified - everything will
+	 * be included.
+	 * 
+	 * @parameter expression="${jacoco.includes}"
+	 */
+	private List<String> includes;
+
+	/**
+	 * A list of class files to exclude from instrumentation/analysis/reports.
+	 * May use wildcard characters (* and ?).
+	 * 
+	 * @parameter expression="${jacoco.excludes}"
+	 */
+	private List<String> excludes;
+
+	/**
+	 * Flag used to suppress execution.
+	 * 
+	 * @parameter expression="${jacoco.skip}" default-value="false"
+	 */
+	private boolean skip;
+
+	/**
+	 * Maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * Doxia Site Renderer.
+	 * 
+	 * @component
+	 */
+	private Renderer siteRenderer;
+
 	private SessionInfoStore sessionInfoStore;
 
 	private ExecutionDataStore executionDataStore;
 
+	public String getOutputName() {
+		return "jacoco/index";
+	}
+
+	public String getName(Locale locale) {
+		return "JaCoCo";
+	}
+
+	public String getDescription(Locale locale) {
+		return "JaCoCo Test Coverage Report.";
+	}
+
 	@Override
-	protected void executeMojo() {
+	public boolean isExternalReport() {
+		return true;
+	}
+
+	@Override
+	protected String getOutputDirectory() {
+		return outputDirectory.getAbsolutePath();
+	}
+
+	@Override
+	protected MavenProject getProject() {
+		return project;
+	}
+
+	@Override
+	protected Renderer getSiteRenderer() {
+		return siteRenderer;
+	}
+
+	protected List<String> getIncludes() {
+		return includes;
+	}
+
+	protected List<String> getExcludes() {
+		return excludes;
+	}
+
+	@Override
+	public void setReportOutputDirectory(File reportOutputDirectory) {
+		if (reportOutputDirectory != null
+				&& !reportOutputDirectory.getAbsolutePath().endsWith("jacoco")) {
+			outputDirectory = new File(reportOutputDirectory, "jacoco");
+		} else {
+			outputDirectory = reportOutputDirectory;
+		}
+	}
+
+	@Override
+	public boolean canGenerateReport() {
+		if ("pom".equals(project.getPackaging())) {
+			getLog().info(
+					"Skipping JaCoCo for project with packaging type 'pom'");
+			return false;
+		}
+		if (skip) {
+			getLog().info("Skipping JaCoCo execution");
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * This method is called when the report generation is invoked directly as a
+	 * standalone Mojo.
+	 */
+	@Override
+	public void execute() throws MojoExecutionException {
+		if (!canGenerateReport()) {
+			return;
+		}
+		try {
+			executeReport(Locale.getDefault());
+		} catch (MavenReportException e) {
+			throw new MojoExecutionException("An error has occurred in "
+					+ getName(Locale.ENGLISH) + " report generation.", e);
+		}
+	}
+
+	@Override
+	protected void executeReport(Locale locale) throws MavenReportException {
 		try {
 			loadExecutionData();
 		} catch (final IOException e) {
