10a3ed3d5af2cbfbec2b35405d8d9420a9bf8776
Marc R. Hoffmann <hoffmann@mountainminds.com>
2012-01-01 12:03:13 +0000

    Update copyright year to 2012.

--- org.jacoco.agent.rt.test/src/org/jacoco/agent/rt/controller/TcpClientControllerTest.java
+++ org.jacoco.agent.rt.test/src/org/jacoco/agent/rt/controller/TcpClientControllerTest.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
 /*******************************************************************************
- * Copyright (c) 2009, 2011 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) Copyright (c) Copyright (c) 2009, 2012 Mountainminds GmbH & Co. KG and Contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
