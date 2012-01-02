61e905db9ae03f604fbc38890dce997c05559d0a
Marc R. Hoffmann <hoffmann@mountainminds.com>
2012-01-02 18:25:52 +0000

    Update copyright year to 2012.

--- org.jacoco.report.test/src/org/jacoco/report/JavaNamesTest.java
+++ org.jacoco.report.test/src/org/jacoco/report/JavaNamesTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) Copyright (c) Copyright (c) 2009, 2012 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) 2009, 2012 Mountainminds GmbH & Co. KG and Contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
