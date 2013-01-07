a0e67db5f06e13323c1df79369040ae4927bf20f
Marc R. Hoffmann <hoffmann@mountainminds.com>
2013-01-07 23:12:33 +0100

    Fix build and instrument Ant task for Windows, reported by Martin Robertson.

--- org.jacoco.ant/src/org/jacoco/ant/InstrumentTask.java
+++ org.jacoco.ant/src/org/jacoco/ant/InstrumentTask.java
@@ -80,21 +80,23 @@ public class InstrumentTask extends Task {
 
 	private int instrument(final Instrumenter instrumenter,
 			final Resource resource) {
-		InputStream input = null;
-		OutputStream output = null;
 		final File file = new File(destdir, resource.getName());
 		file.getParentFile().mkdirs();
 		try {
-			input = resource.getInputStream();
-			output = new FileOutputStream(file);
-			return instrumenter.instrumentAll(input, output);
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				input = resource.getInputStream();
+				output = new FileOutputStream(file);
+				return instrumenter.instrumentAll(input, output);
+			} finally {
+				FileUtils.close(input);
+				FileUtils.close(output);
+			}
 		} catch (final Exception e) {
 			file.delete();
 			throw new BuildException(format("Error while instrumenting %s",
 					resource), e, getLocation());
-		} finally {
-			FileUtils.close(input);
-			FileUtils.close(output);
 		}
 	}
 }
