b61ed960ea01616297aea59d112ee4cd60ae6a76
Marc R. Hoffmann <hoffmann@mountainminds.com>
2013-01-20 13:14:42 +0100

    Update JaCoCo trunk site from latest snapshot build.

--- org.jacoco.build/publish.sh
+++ org.jacoco.build/publish.sh
@@ -6,8 +6,6 @@ if [ "$1" = "" ]; then
 fi
 
 USERNAME=$1
-BASEDIR="$(cd "`dirname $0`" && pwd)"
 
-scp $BASEDIR/../jacoco/target/jacoco-*.zip $USERNAME,eclemma@web.sourceforge.net:/home/frs/project/e/ec/eclemma/07_JaCoCo/trunk
 ssh $USERNAME,eclemma@shell.sourceforge.net create
-ssh $USERNAME,eclemma@shell.sourceforge.net '/home/project-web/eclemma/scripts/publish-jacoco-trunk.sh'
+ssh $USERNAME,eclemma@shell.sourceforge.net '/home/project-web/eclemma/scripts/publish-jacoco-snapshot.sh'
