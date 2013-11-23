f9457f160226468e5f080be188304db05c721423
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-11-23 20:41:31 +0100

    Use extend for integration-tests, fix contributor name

--- org.jacoco.examples/build/src/test/java/org/jacoco/examples/parser/ExpressionParserIT.java
+++ org.jacoco.examples/build/src/test/java/org/jacoco/examples/parser/ExpressionParserIT.java
@@ -6,52 +6,10 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Marc R. Hoffmann - initial API and implementation
+ *    Mirko Friedenhagen - initial
  *    
  *******************************************************************************/
 package org.jacoco.examples.parser;
 
-import java.io.IOException;
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class ExpressionParserIT {
-
-	@Test
-	public void expression1() throws IOException {
-		assertExpression("2 * 3 + 4", 10);
-	}
-
-	@Test
-	public void expression2() throws IOException {
-		assertExpression("2 + 3 * 4", 14);
-	}
-
-	@Test
-	public void expression3() throws IOException {
-		assertExpression("(2 + 3) * 4", 20);
-	}
-
-	@Test
-	public void expression4() throws IOException {
-		assertExpression("2 * 2 * 2 * 2", 16);
-	}
-
-	@Test
-	public void expression5() throws IOException {
-		assertExpression("1 + 2 + 3 + 4", 10);
-	}
-
-	@Test
-	public void expression6() throws IOException {
-		assertExpression("2 * 3 + 2 * 5", 16);
-	}
-
-	private static void assertExpression(final String expression,
-			final double expected) throws IOException {
-		final ExpressionParser parser = new ExpressionParser(expression);
-		final double actual = parser.parse().evaluate();
-		assertEquals("expression", expected, actual, 0.0);
-	}
-
+public class ExpressionParserIT extends ExpressionParserTest {
 }
