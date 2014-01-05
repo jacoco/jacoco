283abfa148b749678924b5e75eabd35a2d58f9f8
Marc R. Hoffmann <hoffmann@mountainminds.com>
2014-01-05 21:17:12 +0100

    Happy New Year 2014!

--- org.jacoco.report/src/org/jacoco/report/internal/html/resources/sort.js
+++ org.jacoco.report/src/org/jacoco/report/internal/html/resources/sort.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) 2009, 2014 Mountainminds GmbH & Co. KG and Contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *    Marc R. Hoffmann - initial API and implementation
  *    
- * $Id: $
  *******************************************************************************/
 
 (function () {
