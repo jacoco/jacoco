58c93d27bc67dc0d0a58c66aa53c27b2fe38a5db
Marc Pawlowsky <marcpawl@localhost.localdomain>
2015-10-06 23:16:45 -0400

    Dual licence (Apache&EPL) annotations module.

--- org.jacoco.annotations/src/org/jacoco/annotations/TreatAsCovered.java
+++ org.jacoco.annotations/src/org/jacoco/annotations/TreatAsCovered.java
@@ -7,7 +7,22 @@
  *
  * Contributors:
  *    Marc Pawlowsky - initial API and implementation
- *    
+ *
+ * Copyright 2015 Marc Pawlowsky and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This work is dual licensed, you may choose between either license.   
  *******************************************************************************/
 
 package org.jacoco.annotations;
