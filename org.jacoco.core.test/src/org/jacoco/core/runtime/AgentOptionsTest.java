3701096e3a409f18fdaa0facb2c809f0920f7708
jochenberger <fooberger@gmail.com>
2015-11-12 12:12:26 +0100

    Add test for #358

--- org.jacoco.core.test/src/org/jacoco/core/runtime/AgentOptionsTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/runtime/AgentOptionsTest.java
@@ -435,5 +435,13 @@ public class AgentOptionsTest {
 				String.format("-javaagent:%s= a b c",
 						defaultAgentJarFile.toString()), vmArgument);
 	}
+	
+	@Test // issue #358
+	public void testDestFileWithComma() {
+		AgentOptions options = new AgentOptions();
+
+		AgentOptions options = new AgentOptions("destfile=build/jacoco/foo, bar.exec");
+		assertEquals("build/jacoco/foo, bar.exec", options.getDestfile());
+	}
 
 }
