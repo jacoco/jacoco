3cf1d3cc80c6894a3c13724fe14f70d991011eb1
Evgeny Mandrikov <Godin@users.noreply.github.com>
2016-05-20 23:26:00 +0200

    Add experimental support for Java 9 class files (#406)

---
+++ jacoco-maven-plugin.test/it/it-java9/src/main/java/Example.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2016 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+public class Example {
+
+  public void sayHello(String name) {
+    // http://openjdk.java.net/jeps/280
+    System.out.println("Hello, " + name);
+  }
+
+}
