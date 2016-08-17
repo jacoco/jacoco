0347ba5dd299e5b3e5c69f85273675320e13d946
Marc R. Hoffmann <hoffmann@mountainminds.com>
2016-08-17 08:29:03 +0200

    Review: Another try to work around Java 9-ea

--- org.jacoco.core.test/src-java8/org/jacoco/core/test/validation/BadCycleInterfaceTest.java
+++ org.jacoco.core.test/src-java8/org/jacoco/core/test/validation/BadCycleInterfaceTest.java
@@ -26,13 +26,11 @@ public class BadCycleInterfaceTest extends ValidationTestBase {
 
 	@Test
 	public void test() throws Exception {
-		if (System.getProperty("java.version").startsWith("9-ea")) {
+		if (!System.getProperty("java.version").startsWith("9-ea")) {
 			// JDK-9042842
-			assertLine("childclinit", ICounter.NOT_COVERED);
-		} else {
 			assertLine("baseclinit", ICounter.FULLY_COVERED);
-			assertLine("childclinit", ICounter.FULLY_COVERED);
 		}
+		assertLine("childclinit", ICounter.FULLY_COVERED);
 		assertLine("childdefault", ICounter.FULLY_COVERED);
 		assertLine("childstatic", ICounter.FULLY_COVERED);
 	}
