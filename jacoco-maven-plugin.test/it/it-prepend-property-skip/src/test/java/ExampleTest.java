22ab985bb01a55cddf2a4af53ae0bed0e300b7d4
Evgeny Mandrikov <mandrikov@gmail.com>
2017-02-03 21:46:34 +0100

    "prepare-agent" should not overwrite property if execution skipped

---
+++ jacoco-maven-plugin.test/it/it-prepend-property-skip/src/test/java/ExampleTest.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2017 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ExampleTest {
+
+  @Test
+  public void test() {
+    assertNotNull(System.getProperty("foo"));
+  }
+
+}
