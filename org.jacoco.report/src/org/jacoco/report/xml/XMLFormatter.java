jacoco/jacoco

commit b4e9b0ae88cc7b2b604ef44e9286f322acd079ae

Cleanup internal XML generation framework (#652)
* Combine XMLElement and XMLDocument to allow single schema specific
subclass
* Add subclass for JaCoCo report XML schema
* Remove mostly unused return values
* Remove unused Writer output
* Cleanup unit tests
* Use new test naming conventions

org.jacoco.report/src/org/jacoco/report/xml/XMLFormatter.java
@@ -19,12 +19,11 @@ import java.util.List;
 import org.jacoco.core.analysis.IBundleCoverage;
 import org.jacoco.core.data.ExecutionData;
 import org.jacoco.core.data.SessionInfo;
+import org.jacoco.report.IReportGroupVisitor;
 import org.jacoco.report.IReportVisitor;
 import org.jacoco.report.ISourceFileLocator;
-import org.jacoco.report.internal.AbstractGroupVisitor;
+import org.jacoco.report.internal.xml.ReportElement;
 import org.jacoco.report.internal.xml.XMLCoverageWriter;
-import org.jacoco.report.internal.xml.XMLDocument;
-import org.jacoco.report.internal.xml.XMLElement;
 import org.jacoco.report.internal.xml.XMLGroupVisitor;
 
 /**
@@ -59,15 +58,11 @@ public class XMLFormatter {
 	 */
 	public IReportVisitor createVisitor(final OutputStream output)
 			throws IOException {
-		final XMLElement root = new XMLDocument("report", PUBID, SYSTEM,
-				outputEncoding, true, output);
-		class RootVisitor extends XMLGroupVisitor implements IReportVisitor {
+		class RootVisitor implements IReportVisitor {
 
-			RootVisitor(final XMLElement element) throws IOException {
-				super(element, null);
-			}
-
+			private ReportElement report;
 			private List<SessionInfo> sessionInfos;
+			private XMLGroupVisitor groupVisitor;
 
 			public void visitInfo(final List<SessionInfo> sessionInfos,
 					final Collection<ExecutionData> executionData)
@@ -75,36 +70,35 @@ public class XMLFormatter {
 				this.sessionInfos = sessionInfos;
 			}
 
-			@Override
-			protected void handleBundle(final IBundleCoverage bundle,
+			public void visitBundle(final IBundleCoverage bundle,
 					final ISourceFileLocator locator) throws IOException {
-				writeHeader(bundle.getName());
-				XMLCoverageWriter.writeBundle(bundle, element);
+				createRootElement(bundle.getName());
+				XMLCoverageWriter.writeBundle(bundle, report);
 			}
 
-			@Override
-			protected AbstractGroupVisitor handleGroup(final String name)
+			public IReportGroupVisitor visitGroup(final String name)
 					throws IOException {
-				writeHeader(name);
-				return new XMLGroupVisitor(element, name);
+				createRootElement(name);
+				groupVisitor = new XMLGroupVisitor(report, name);
+				return groupVisitor;
 			}
 
-			private void writeHeader(final String name) throws IOException {
-				element.attr("name", name);
+			private void createRootElement(final String name)
+					throws IOException {
+				report = new ReportElement(name, output, outputEncoding);
 				for (final SessionInfo i : sessionInfos) {
-					final XMLElement sessioninfo = root.element("sessioninfo");
-					sessioninfo.attr("id", i.getId());
-					sessioninfo.attr("start", i.getStartTimeStamp());
-					sessioninfo.attr("dump", i.getDumpTimeStamp());
+					report.sessioninfo(i);
 				}
 			}
 
-			@Override
-			protected void handleEnd() throws IOException {
-				element.close();
+			public void visitEnd() throws IOException {
+				if (groupVisitor != null) {
+					groupVisitor.visitEnd();
+				}
+				report.close();
 			}
 		}
-		return new RootVisitor(root);
+		return new RootVisitor();
 	}
 
 }

org.jacoco.cli/src/org/jacoco/cli/internal/XmlDocumentation.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.jacoco.cli.internal.commands.AllCommands;
-import org.jacoco.report.internal.xml.XMLDocument;
 import org.jacoco.report.internal.xml.XMLElement;
 import org.kohsuke.args4j.spi.OptionHandler;
 
@@ -65,8 +64,8 @@ public final class XmlDocumentation {
 		final File file = new File(args[0]);
 		file.getParentFile().mkdirs();
 
-		final XMLElement root = new XMLDocument("documentation", null, null,
-				"UTF-8", true, new FileOutputStream(file));
+		final XMLElement root = new XMLElement("documentation", null, null,
+				true, "UTF-8", new FileOutputStream(file));
 
 		for (final Command c : AllCommands.get()) {
 			writeCommand(c, root);

org.jacoco.report.test/src/org/jacoco/report/ReportStructureTestDriver.java
@@ -69,7 +69,7 @@ public class ReportStructureTestDriver {
 		m.increment(CounterImpl.getInstance(3, 5), CounterImpl.COUNTER_0_0, 1);
 		m.increment(CounterImpl.getInstance(3, 5),
 				CounterImpl.getInstance(1, 2), 2);
-		m.increment(CounterImpl.getInstance(4, 5), CounterImpl.COUNTER_0_0, 3);
+		m.increment(CounterImpl.getInstance(4, 5), CounterImpl.COUNTER_0_0, 4);
 		m.incrementMethodCounter();
 		methodCoverage = m;
 
@@ -119,7 +119,8 @@ public class ReportStructureTestDriver {
 		reportVisitor.visitEnd();
 	}
 
-	public void sendBundle(IReportGroupVisitor groupVisitor) throws IOException {
+	public void sendBundle(IReportGroupVisitor groupVisitor)
+			throws IOException {
 		groupVisitor.visitBundle(bundleCoverage, sourceFileLocator);
 	}
 

org.jacoco.report.test/src/org/jacoco/report/internal/html/HTMLDocumentTest.java
@@ -1,85 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2009, 2018 Mountainminds GmbH & Co. KG and Contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Marc R. Hoffmann - initial API and implementation
- *    
- *******************************************************************************/
-package org.jacoco.report.internal.html;
-
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-
-import org.junit.Test;
-
-/**
- * Unit tests for {@link HTMLDocument}.
- */
-public class HTMLDocumentTest {
-
-	@Test
-	public void testWriter() throws IOException {
-		StringWriter buffer = new StringWriter();
-		new HTMLDocument(buffer, "UTF-8").close();
-		assertEquals(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-						+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-						+ "<html xmlns=\"http://www.w3.org/1999/xhtml\"/>",
-				buffer.toString());
-	}
-
-	@Test
-	public void testStream() throws IOException {
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		new HTMLDocument(buffer, "UTF-8").close();
-		assertEquals(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-						+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-						+ "<html xmlns=\"http://www.w3.org/1999/xhtml\"/>",
-				buffer.toString("UTF-8"));
-	}
-
-	@Test
-	public void testHead() throws IOException {
-		StringWriter buffer = new StringWriter();
-		final HTMLDocument doc = new HTMLDocument(buffer, "UTF-8");
-		doc.head();
-		doc.close();
-		assertEquals(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-						+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-						+ "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head/></html>",
-				buffer.toString());
-	}
-
-	@Test
-	public void testBody() throws IOException {
-		StringWriter buffer = new StringWriter();
-		final HTMLDocument doc = new HTMLDocument(buffer, "UTF-8");
-		doc.body();
-		doc.close();
-		assertEquals(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-						+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-						+ "<html xmlns=\"http://www.w3.org/1999/xhtml\"><body/></html>",
-				buffer.toString());
-	}
-
-	@Test
-	public void testMinimalHTMLDocument() throws Exception {
-		StringWriter buffer = new StringWriter();
-		final HTMLDocument doc = new HTMLDocument(buffer, "UTF-8");
-		doc.head().title();
-		doc.body();
-		doc.close();
-		new HTMLSupport().parse(buffer.toString());
-	}
-
-}

org.jacoco.report.test/src/org/jacoco/report/internal/html/HTMLElementTest.java
@@ -13,8 +13,8 @@ package org.jacoco.report.internal.html;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.StringWriter;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,218 +24,215 @@ import org.junit.Test;
  */
 public class HTMLElementTest {
 
-	private StringWriter buffer;
+	private static final String PREFIX = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">";
+
+	private static final String SUFFIX = "</html>";
+
+	private ByteArrayOutputStream buffer;
 
 	private HTMLElement root;
 
 	@Before
-	public void setUp() throws IOException {
-		buffer = new StringWriter();
-		root = new HTMLElement(buffer, "root") {
-			{
-				beginOpenTag();
-			}
-		};
+	public void setup() throws IOException {
+		buffer = new ByteArrayOutputStream();
+		root = new HTMLElement(buffer, "UTF-8");
 	}
 
 	@Test
-	public void testMeta() throws IOException {
+	public void should_create_minimal_valid_html_document() throws Exception {
+		root.head().title();
+		root.body();
+		root.close();
+		new HTMLSupport().parse(buffer);
+	}
+
+	@Test
+	public void head_should_create_head_tag() throws IOException {
+		root.head();
+		assertContent("<head/>");
+	}
+
+	@Test
+	public void meta_should_create_meta_tag_with_attributes()
+			throws IOException {
 		root.meta("key", "value");
-		root.close();
-		assertEquals(
-				"<root><meta http-equiv=\"key\" content=\"value\"/></root>",
-				buffer.toString());
+		assertContent("<meta http-equiv=\"key\" content=\"value\"/>");
 	}
 
 	@Test
-	public void testLink() throws IOException {
+	public void body_should_create_body_tag() throws IOException {
+		root.body();
+		assertContent("<body/>");
+	}
+
+	@Test
+	public void link_should_create_link_tag_with_attributes()
+			throws IOException {
 		root.link("stylesheet", "style.css", "text/css");
-		root.close();
-		assertEquals(
-				"<root><link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/></root>",
-				buffer.toString());
+		assertContent(
+				"<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>");
 	}
 
 	@Test
-	public void testTitle() throws IOException {
+	public void title_should_create_title_tag() throws IOException {
 		root.title();
-		root.close();
-		assertEquals("<root><title/></root>", buffer.toString());
+		assertContent("<title/>");
 	}
 
 	@Test
-	public void testH1() throws IOException {
+	public void h1_should_create_h1_tag() throws IOException {
 		root.h1();
-		root.close();
-		assertEquals("<root><h1/></root>", buffer.toString());
+		assertContent("<h1/>");
 	}
 
 	@Test
-	public void testP() throws IOException {
+	public void p_should_create_p_tag() throws IOException {
 		root.p();
-		root.close();
-		assertEquals("<root><p/></root>", buffer.toString());
+		assertContent("<p/>");
 	}
 
 	@Test
-	public void testSpan1() throws IOException {
+	public void span_should_create_span_tag() throws IOException {
 		root.span();
-		root.close();
-		assertEquals("<root><span/></root>", buffer.toString());
+		assertContent("<span/>");
 	}
 
 	@Test
-	public void testSpan2() throws IOException {
+	public void span_should_create_span_tag_with_class_attribute()
+			throws IOException {
 		root.span("abc");
-		root.close();
-		assertEquals("<root><span class=\"abc\"/></root>", buffer.toString());
+		assertContent("<span class=\"abc\"/>");
 	}
 
 	@Test
-	public void testSpan3() throws IOException {
+	public void span_should_create_span_tag_with_class_and_id_attribute()
+			throws IOException {
 		root.span("abc", "xy");
-		root.close();
-		assertEquals("<root><span class=\"abc\" id=\"xy\"/></root>",
-				buffer.toString());
+		assertContent("<span class=\"abc\" id=\"xy\"/>");
 	}
 
 	@Test
-	public void testPre() throws IOException {
+	public void pre_should_create_pre_tag_with_class_attribute()
+			throws IOException {
 		root.pre("mystyle");
-		root.close();
-		assertEquals("<root><pre class=\"mystyle\"/></root>", buffer.toString());
+		assertContent("<pre class=\"mystyle\"/>");
 	}
 
 	@Test
-	public void testDiv() throws IOException {
+	public void div_should_create_div_tag_with_class_attribute()
+			throws IOException {
 		root.div("mystyle");
-		root.close();
-		assertEquals("<root><div class=\"mystyle\"/></root>", buffer.toString());
+		assertContent("<div class=\"mystyle\"/>");
 	}
 
 	@Test
-	public void testCode() throws IOException {
+	public void code_should_create_code_tag() throws IOException {
 		root.code().text("0xCAFEBABE");
-		root.close();
-		assertEquals("<root><code>0xCAFEBABE</code></root>", buffer.toString());
+		assertContent("<code>0xCAFEBABE</code>");
 	}
 
 	@Test
-	public void testA1() throws IOException {
+	public void a_should_create_a_tag_with_href_attribute() throws IOException {
 		root.a("http://www.jacoco.org/");
-		root.close();
-		assertEquals("<root><a href=\"http://www.jacoco.org/\"/></root>",
-				buffer.toString());
+		assertContent("<a href=\"http://www.jacoco.org/\"/>");
 	}
 
 	@Test
-	public void testA2() throws IOException {
+	public void a_should_create_a_tag_with_href_and_class_attribute()
+			throws IOException {
 		root.a("http://www.jacoco.org/", "extern");
-		root.close();
-		assertEquals(
-				"<root><a href=\"http://www.jacoco.org/\" class=\"extern\"/></root>",
-				buffer.toString());
+		assertContent("<a href=\"http://www.jacoco.org/\" class=\"extern\"/>");
 	}
 
 	@Test
-	public void testALinkable1() throws IOException {
+	public void a_should_create_span_tag_when_no_link_is_given()
+			throws IOException {
 		root.a(new LinkableStub(null, "here", null), null);
-		root.close();
-		assertEquals("<root><span>here</span></root>", buffer.toString());
+		assertContent("<span>here</span>");
 	}
 
 	@Test
-	public void testALinkable2() throws IOException {
+	public void a_should_create_span_tag_with_class_attribute_when_no_link_is_given()
+			throws IOException {
 		root.a(new LinkableStub(null, "here", "blue"), null);
-		root.close();
-		assertEquals("<root><span class=\"blue\">here</span></root>",
-				buffer.toString());
+		assertContent("<span class=\"blue\">here</span>");
 	}
 
 	@Test
-	public void testALinkable3() throws IOException {
+	public void a_should_create_a_tag_when_link_is_given() throws IOException {
 		root.a(new LinkableStub("index.html", "here", null), null);
-		root.close();
-		assertEquals("<root><a href=\"index.html\">here</a></root>",
-				buffer.toString());
+		assertContent("<a href=\"index.html\">here</a>");
 	}
 
 	@Test
-	public void testALinkable4() throws IOException {
+	public void a_should_create_a_tag_with_class_attribute_when_link_is_given()
+			throws IOException {
 		root.a(new LinkableStub("index.html", "here", "red"), null);
-		root.close();
-		assertEquals(
-				"<root><a href=\"index.html\" class=\"red\">here</a></root>",
-				buffer.toString());
+		assertContent("<a href=\"index.html\" class=\"red\">here</a>");
 	}
 
 	@Test
-	public void testTable() throws IOException {
+	public void table_should_create_table_tag_with_attributes()
+			throws IOException {
 		root.table("tablestyle");
-		root.close();
-		assertEquals(
-				"<root><table class=\"tablestyle\" cellspacing=\"0\"/></root>",
-				buffer.toString());
+		assertContent("<table class=\"tablestyle\" cellspacing=\"0\"/>");
 	}
 
 	@Test
-	public void testThead() throws IOException {
+	public void thead_should_create_thead_tag() throws IOException {
 		root.thead();
-		root.close();
-		assertEquals("<root><thead/></root>", buffer.toString());
+		assertContent("<thead/>");
 	}
 
 	@Test
-	public void testTfoot() throws IOException {
+	public void tfoot_should_create_tfoot_tag() throws IOException {
 		root.tfoot();
-		root.close();
-		assertEquals("<root><tfoot/></root>", buffer.toString());
+		assertContent("<tfoot/>");
 	}
 
 	@Test
-	public void testTbody() throws IOException {
+	public void tbody_should_create_tbody_tag() throws IOException {
 		root.tbody();
-		root.close();
-		assertEquals("<root><tbody/></root>", buffer.toString());
+		assertContent("<tbody/>");
 	}
 
 	@Test
-	public void testTr() throws IOException {
+	public void tr_should_create_tr_tag() throws IOException {
 		root.tr();
-		root.close();
-		assertEquals("<root><tr/></root>", buffer.toString());
+		assertContent("<tr/>");
 	}
 
 	@Test
-	public void testTd1() throws IOException {
+	public void td_should_create_td_tag() throws IOException {
 		root.td();
-		root.close();
-		assertEquals("<root><td/></root>", buffer.toString());
+		assertContent("<td/>");
 	}
 
 	@Test
-	public void testTd2() throws IOException {
+	public void td_should_create_td_tag_with_class_attribute()
+			throws IOException {
 		root.td("mystyle");
-		root.close();
-		assertEquals("<root><td class=\"mystyle\"/></root>", buffer.toString());
+		assertContent("<td class=\"mystyle\"/>");
 	}
 
 	@Test
-	public void testImg() throws IOException {
+	public void img_should_create_img_tag_with_attributes() throws IOException {
 		root.img("sample.gif", 16, 32, "Hello");
-		root.close();
-		assertEquals(
-				"<root><img src=\"sample.gif\" width=\"16\" height=\"32\" title=\"Hello\" alt=\"Hello\"/></root>",
-				buffer.toString());
+		assertContent(
+				"<img src=\"sample.gif\" width=\"16\" height=\"32\" title=\"Hello\" alt=\"Hello\"/>");
 	}
 
 	@Test
-	public void testScript() throws IOException {
-		root.script("text/javascript", "file.js");
+	public void script_should_create_script_tag_with_attributes()
+			throws IOException {
+		root.script("file.js");
+		assertContent(
+				"<script type=\"text/javascript\" src=\"file.js\"></script>");
+	}
+
+	private void assertContent(String expected) throws IOException {
 		root.close();
-		assertEquals(
-				"<root><script type=\"text/javascript\" src=\"file.js\"></script></root>",
-				buffer.toString());
+		assertEquals(PREFIX + expected + SUFFIX, buffer.toString("UTF-8"));
 	}
 
 }

org.jacoco.report.test/src/org/jacoco/report/internal/html/page/SourceHighlighterTest.java
@@ -13,14 +13,13 @@ package org.jacoco.report.internal.html.page;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
 import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Locale;
 
 import org.jacoco.core.analysis.ICoverageNode.ElementType;
 import org.jacoco.core.internal.analysis.CounterImpl;
 import org.jacoco.core.internal.analysis.SourceNodeImpl;
-import org.jacoco.report.internal.html.HTMLDocument;
 import org.jacoco.report.internal.html.HTMLElement;
 import org.jacoco.report.internal.html.HTMLSupport;
 import org.jacoco.report.internal.html.resources.Styles;
@@ -35,9 +34,9 @@ public class SourceHighlighterTest {
 
 	private HTMLSupport htmlSupport;
 
-	private StringWriter buffer;
+	private ByteArrayOutputStream buffer;
 
-	private HTMLDocument html;
+	private HTMLElement html;
 
 	private HTMLElement parent;
 
@@ -49,8 +48,8 @@ public class SourceHighlighterTest {
 	public void setup() throws Exception {
 		htmlSupport = new HTMLSupport();
 		source = new SourceNodeImpl(ElementType.SOURCEFILE, "Foo.java");
-		buffer = new StringWriter();
-		html = new HTMLDocument(buffer, "UTF-8");
+		buffer = new ByteArrayOutputStream();
+		html = new HTMLElement(buffer, "UTF-8");
 		html.head().title();
 		parent = html.body();
 		sourceHighlighter = new SourceHighlighter(Locale.US);
@@ -60,8 +59,7 @@ public class SourceHighlighterTest {
 	public void testDefaultTabWidth() throws Exception {
 		final String src = "\tA";
 		sourceHighlighter.render(parent, source, new StringReader(src));
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 
 		// Assert that we no longer replace tabs with spaces
 		assertEquals("\tA\n", htmlSupport.findStr(doc, "//pre/text()"));
@@ -70,8 +68,7 @@ public class SourceHighlighterTest {
 	@Test
 	public void testDefaultLanguage() throws Exception {
 		sourceHighlighter.render(parent, source, new StringReader(""));
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("source lang-java linenums",
 				htmlSupport.findStr(doc, "//pre/@class"));
 	}
@@ -80,8 +77,7 @@ public class SourceHighlighterTest {
 	public void testSetLanguage() throws Exception {
 		sourceHighlighter.setLanguage("scala");
 		sourceHighlighter.render(parent, source, new StringReader(""));
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("source lang-scala linenums",
 				htmlSupport.findStr(doc, "//pre/@class"));
 	}
@@ -94,8 +90,7 @@ public class SourceHighlighterTest {
 		source.increment(CounterImpl.COUNTER_0_1, CounterImpl.COUNTER_0_0, 2);
 		source.increment(CounterImpl.COUNTER_0_1, CounterImpl.COUNTER_0_0, 3);
 		sourceHighlighter.render(parent, source, new StringReader(src));
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals(Styles.NOT_COVERED,
 				htmlSupport.findStr(doc, "//pre/span[text() = 'A']/@class"));
 		assertEquals(Styles.PARTLY_COVERED,
@@ -109,18 +104,16 @@ public class SourceHighlighterTest {
 	@Test
 	public void testHighlightNone() throws Exception {
 		sourceHighlighter.highlight(parent, source.getLine(1), 1);
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("", htmlSupport.findStr(doc, "//pre"));
 	}
 
 	@Test
 	public void testHighlightBranchesFC() throws Exception {
-		source.increment(CounterImpl.COUNTER_0_1,
-				CounterImpl.getInstance(0, 5), 1);
+		source.increment(CounterImpl.COUNTER_0_1, CounterImpl.getInstance(0, 5),
+				1);
 		sourceHighlighter.highlight(parent.pre(null), source.getLine(1), 1);
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("fc bfc", htmlSupport.findStr(doc, "//pre/span/@class"));
 		assertEquals("All 5 branches covered.",
 				htmlSupport.findStr(doc, "//pre/span/@title"));
@@ -128,11 +121,10 @@ public class SourceHighlighterTest {
 
 	@Test
 	public void testHighlightBranchesPC() throws Exception {
-		source.increment(CounterImpl.COUNTER_0_1,
-				CounterImpl.getInstance(2, 3), 1);
+		source.increment(CounterImpl.COUNTER_0_1, CounterImpl.getInstance(2, 3),
+				1);
 		sourceHighlighter.highlight(parent.pre(null), source.getLine(1), 1);
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("pc bpc", htmlSupport.findStr(doc, "//pre/span/@class"));
 		assertEquals("2 of 5 branches missed.",
 				htmlSupport.findStr(doc, "//pre/span/@title"));
@@ -140,14 +132,18 @@ public class SourceHighlighterTest {
 
 	@Test
 	public void testHighlightBranchesNC() throws Exception {
-		source.increment(CounterImpl.COUNTER_0_1,
-				CounterImpl.getInstance(5, 0), 1);
+		source.increment(CounterImpl.COUNTER_0_1, CounterImpl.getInstance(5, 0),
+				1);
 		sourceHighlighter.highlight(parent.pre(null), source.getLine(1), 1);
-		html.close();
-		final Document doc = htmlSupport.parse(buffer.toString());
+		final Document doc = parseDoc();
 		assertEquals("pc bnc", htmlSupport.findStr(doc, "//pre/span/@class"));
 		assertEquals("All 5 branches missed.",
 				htmlSupport.findStr(doc, "//pre/span/@title"));
+	}
+
+	private Document parseDoc() throws Exception {
+		html.close();
+		return htmlSupport.parse(buffer);
 	}
 
 }

commit edd7c5de095fdc1ea6b9364d3dacdee538140b99

Remove unused constants.
Regression introduced by refactoring in #652.

org.jacoco.report/src/org/jacoco/report/xml/XMLFormatter.java
@@ -31,10 +31,6 @@ import org.jacoco.report.internal.xml.XMLGroupVisitor;
  */
 public class XMLFormatter {
 
-	private static final String PUBID = "-//JACOCO//DTD Report 1.1//EN";
-
-	private static final String SYSTEM = "report.dtd";
-
 	private String outputEncoding = "UTF-8";
 
 	/**