0cfd6acdfe5b3134545e832feb3b3985b7574893
Evgeny Mandrikov <mandrikov@gmail.com>
2018-08-09 21:54:26 +0200

    Add source filename to assertion messages
    This simplifies navigation in IDEs.

--- org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
+++ org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
@@ -120,13 +120,15 @@ public abstract class ValidationTestBase {
 	private void assertCoverage(final Line line, final int insnStatus,
 			final int missedBranches, final int coveredBranches) {
 		final ILine coverage = line.getCoverage();
+		final String location = String.format("(%s:%d)",
+				source.getCoverage().getName(), line.getNr());
 
-		String msg = String.format("Instructions in %s: %s", line,
+		String msg = String.format("Instructions in %s: %s", location,
 				line.getText());
 		final int actualStatus = coverage.getInstructionCounter().getStatus();
 		assertEquals(msg, STATUS_NAME[insnStatus], STATUS_NAME[actualStatus]);
 
-		msg = String.format("Branches in %s: %s", line, line.getText());
+		msg = String.format("Branches in %s: %s", location, line.getText());
 		assertEquals(msg,
 				CounterImpl.getInstance(missedBranches, coveredBranches),
 				coverage.getBranchCounter());
