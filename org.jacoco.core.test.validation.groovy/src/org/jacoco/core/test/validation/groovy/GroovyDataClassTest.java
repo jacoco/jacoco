7abdcc0d77dc5cb8e23617b912d67d8007464140
aalmiray <aalmiray@gmail.com>
2018-08-14 21:31:02 +0200

    Add a failing test

--- org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyDataClassTest.java
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyDataClassTest.java
@@ -13,6 +13,7 @@ package org.jacoco.core.test.validation.groovy;
 
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.groovy.targets.GroovyDataClassTarget;
+import org.junit.Test;
 
 /**
  * Test of <code>data class</code>es.
@@ -20,5 +21,14 @@ import org.jacoco.core.test.validation.groovy.targets.GroovyDataClassTarget;
 public class GroovyDataClassTest extends ValidationTestBase {
     public GroovyDataClassTest() {
         super(GroovyDataClassTarget.class);
+    }
+
+    @Test
+    public void test_method_count() {
+        // This test fails in Groovy 2.5.2 and earlier because the
+        // compiler does not at @groovy.transform.Generated to
+        // property getters & setters nor the @ToString and
+        // @EqualsAndHashcode AST transformations
+        assertMethodCount(2);
     }
 }
