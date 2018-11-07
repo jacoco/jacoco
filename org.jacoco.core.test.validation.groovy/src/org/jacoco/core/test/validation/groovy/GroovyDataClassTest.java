470a132fc8001a8c2f76bfe19dba5bc0366fd665
Andres Almiray <aalmiray@yahoo.com>
2018-11-07 23:45:19 +0100

    Add module with validation tests for Groovy (#733)

---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyDataClassTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2018 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andres Almiray - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy;
+
+import org.jacoco.core.test.validation.ValidationTestBase;
+import org.jacoco.core.test.validation.groovy.targets.GroovyDataClassTarget;
+import org.junit.Test;
+
+/**
+ * Test of <code>data class</code>es.
+ */
+public class GroovyDataClassTest extends ValidationTestBase {
+    public GroovyDataClassTest() {
+        super(GroovyDataClassTarget.class);
+    }
+
+    @Test
+    public void test_method_count() {
+        assertMethodCount(1);
+    }
+}
