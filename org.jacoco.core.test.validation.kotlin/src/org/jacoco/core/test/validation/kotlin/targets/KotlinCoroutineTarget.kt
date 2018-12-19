96b549c29f72cc133dacb43a43a70f520b617597
Evgeny Mandrikov <mandrikov@gmail.com>
2018-12-19 18:10:48 +0100

    (WIP) add validation test

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinCoroutineTarget.kt
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2018 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+import kotlinx.coroutines.runBlocking
+import org.jacoco.core.test.validation.targets.Stubs.nop
+
+/**
+ * Test target for coroutines.
+ */
+object KotlinCoroutineTarget {
+
+    suspend fun suspendingFunction() {
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        runBlocking { // assertFullyCovered()
+            nop() // assertFullyCovered()
+            suspendingFunction() // assertFullyCovered()
+            nop() // assertFullyCovered()
+        } // assertFullyCovered()
+
+    }
+
+}
