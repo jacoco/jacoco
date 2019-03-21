89a89d7b8f84a9bdbd21e4ae7e3628ffe0b21b89
Stephan Classen <st.classen@gmx.ch>
2019-03-21 20:25:55 +0100

    add test for grovvy auto externalize AST

---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/targets/GroovyAutoExternalizeClassTarget.groovy
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stephan Classen
+ *    Vadim Bauer
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy.targets
+
+/* This annotation generates the following
+ * - writeExternal()
+ # - readExternal()
+ */
+
+@groovy.transform.AutoExternalize
+class GroovyAutoExternalizeClassTarget { // assertEmpty()
+
+    String first    // assertEmpty()
+    List favItems     // assertEmpty()
+    Date since    // assertEmpty()
+
+    static void main(String[] args) {
+        new GroovyAutoExternalizeClassTarget() // assertFullyCovered()
+    }
+}
