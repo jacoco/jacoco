702fde9311fd64cec8cbb746edf85c7fcdfa4336
ollin <oliver.nautsch@gmail.com>
2019-03-21 21:40:08 +0100

    added integration test for groovy  @AutoClone

---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyAutoCloneClassTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andres Almiray - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy;
+
+import org.jacoco.core.test.validation.ValidationTestBase;
+import org.jacoco.core.test.validation.groovy.targets.GroovyAutoCloneClassTarget;
+import org.jacoco.core.test.validation.groovy.targets.GroovyDataClassTarget;
+import org.junit.Test;
+
+/**
+ * Test of <code>data class</code>es.
+ */
+public class GroovyAutoCloneClassTest extends ValidationTestBase {
+    public GroovyAutoCloneClassTest() {
+        super(GroovyAutoCloneClassTarget.class);
+    }
+
+    @Test
+    public void test_method_count() {
+        assertMethodCount(1);
+    }
+}
