45834440a2220ba5b9df56d6adf04ee148784864
Stephan Classen <st.classen@gmx.ch>
2019-03-21 21:13:09 +0100

    add test for grovvy bindable AST

---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/GroovyBindableClassTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stephan Classen
+ *    Vadim Bauer
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy;
+
+import org.jacoco.core.test.validation.ValidationTestBase;
+import org.jacoco.core.test.validation.groovy.targets.GroovyBindableClassTarget;
+import org.junit.Test;
+
+/**
+ * Test of <code>data class</code>es.
+ */
+public class GroovyBindableClassTest extends ValidationTestBase {
+    public GroovyBindableClassTest() {
+        super(GroovyBindableClassTarget.class);
+    }
+
+    @Test
+    public void test_method_count() {
+        assertMethodCount(1);
+    }
+}
---
+++ org.jacoco.core.test.validation.groovy/src/org/jacoco/core/test/validation/groovy/targets/GroovyBindableClassTarget.groovy
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stephan Classen
+ *    Vadim Bauer
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.groovy.targets
+
+/* This annotation generates the following
+ * - getters and setters
+ * - 2 versions of addPropertyChangeListener()
+ * - 2 versions of removePropertyChangeListener()
+ * - 2 versions of getPropertyChangeListeners()
+ * - firePropertyChange()
+ */
+
+class GroovyBindableClassTarget { // assertEmpty()
+
+    @groovy.beans.Bindable
+    String firstName    // assertEmpty()
+
+    @groovy.beans.Bindable
+    def zipCode    // assertEmpty()
+
+    static void main(String[] args) {
+        new GroovyBindableClassTarget() // assertFullyCovered()
+    }
+}
