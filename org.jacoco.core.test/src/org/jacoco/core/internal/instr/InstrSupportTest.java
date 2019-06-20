6be65643ccd83d9cb5f87274a192c8296cda8d7f
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2019-06-20 22:56:53 +0200

    Add experimental support for Java 14 class files (#897)

--- org.jacoco.core.test/src/org/jacoco/core/internal/instr/InstrSupportTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/instr/InstrSupportTest.java
@@ -46,8 +46,8 @@ public class InstrSupportTest {
 	}
 
 	@Test
-	public void classReaderFor_should_read_java_13_class() {
-		final byte[] bytes = createJava13Class();
+	public void classReaderFor_should_read_java_14_class() {
+		final byte[] bytes = createJava14Class();
 
 		final ClassReader classReader = InstrSupport.classReaderFor(bytes);
 
@@ -56,16 +56,16 @@ public class InstrSupportTest {
 			public void visit(final int version, final int access,
 					final String name, final String signature,
 					final String superName, final String[] interfaces) {
-				assertEquals(Opcodes.V12 + 1, version);
+				assertEquals(Opcodes.V13 + 1, version);
 			}
 		}, 0);
 
-		assertArrayEquals(createJava13Class(), bytes);
+		assertArrayEquals(createJava14Class(), bytes);
 	}
 
-	private static byte[] createJava13Class() {
+	private static byte[] createJava14Class() {
 		final ClassWriter cw = new ClassWriter(0);
-		cw.visit(Opcodes.V12 + 1, 0, "Foo", null, "java/lang/Object", null);
+		cw.visit(Opcodes.V13 + 1, 0, "Foo", null, "java/lang/Object", null);
 		cw.visitEnd();
 		return cw.toByteArray();
 	}
@@ -127,7 +127,8 @@ public class InstrSupportTest {
 		assertTrue(InstrSupport.needsFrames(Opcodes.V10));
 		assertTrue(InstrSupport.needsFrames(Opcodes.V11));
 		assertTrue(InstrSupport.needsFrames(Opcodes.V12));
-		assertTrue(InstrSupport.needsFrames(Opcodes.V12 + 1));
+		assertTrue(InstrSupport.needsFrames(Opcodes.V13));
+		assertTrue(InstrSupport.needsFrames(Opcodes.V13 + 1));
 
 		assertTrue(InstrSupport.needsFrames(0x0100));
 	}
