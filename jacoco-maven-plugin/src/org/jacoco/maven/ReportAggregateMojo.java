702b62de5ae05f078acebc9738d566ded4e74933
Evgeny Mandrikov <mandrikov@gmail.com>
2019-09-20 14:14:30 +0200

    (WIP) update headers

--- jacoco-maven-plugin/src/org/jacoco/maven/ReportAggregateMojo.java
+++ jacoco-maven-plugin/src/org/jacoco/maven/ReportAggregateMojo.java
@@ -1,9 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    John Oliver, Marc R. Hoffmann, Jan Wloka - initial API and implementation
