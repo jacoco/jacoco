5ae7caf68d5b520bebc3e207f1b440a43c0fba15
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2019-09-23 15:56:24 +0200

    Update license to EPL version 2.0 (#943)

--- LICENSE.md
+++ LICENSE.md
@@ -6,8 +6,8 @@ Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
 The JaCoCo Java Code Coverage Library and all included documentation is made
 available by Mountainminds GmbH & Co. KG, Munich. Except indicated below, the
 Content is provided to you under the terms and conditions of the Eclipse Public
-License Version 1.0 ("EPL"). A copy of the EPL is available at
-[http://www.eclipse.org/legal/epl-v10.html](http://www.eclipse.org/legal/epl-v10.html).
+License Version 2.0 ("EPL"). A copy of the EPL is available at
+[https://www.eclipse.org/legal/epl-2.0/](https://www.eclipse.org/legal/epl-2.0/).
 
 Please visit
 [http://www.jacoco.org/jacoco/trunk/doc/license.html](http://www.jacoco.org/jacoco/trunk/doc/license.html)
--- org.jacoco.examples.test/src/org/jacoco/examples/CoreTutorialTest.java
+++ org.jacoco.examples.test/src/org/jacoco/examples/CoreTutorialTest.java
@@ -1,9 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    Marc R. Hoffmann - initial API and implementation
@@ -30,9 +31,9 @@ public class CoreTutorialTest {
 
 		console.expect(containsLine("0 of 3 methods missed"));
 		console.expect(containsLine("1 of 5 complexity missed"));
-		console.expect(containsLine("Line 46: "));
-		console.expect(containsLine("Line 47: green"));
-		console.expect(containsLine("Line 48: yellow"));
-		console.expect(containsLine("Line 49: red"));
+		console.expect(containsLine("Line 47: "));
+		console.expect(containsLine("Line 48: green"));
+		console.expect(containsLine("Line 49: yellow"));
+		console.expect(containsLine("Line 50: red"));
 	}
 }
