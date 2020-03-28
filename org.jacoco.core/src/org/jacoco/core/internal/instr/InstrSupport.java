4d222a863e936a81a7d542e8ea33d0b2c6b0e263
Evgeny Mandrikov <mandrikov@gmail.com>
2020-03-28 18:13:03 +0100

    (WIP) Upgrade ASM to 8.0

--- org.jacoco.core/src/org/jacoco/core/internal/instr/InstrSupport.java
+++ org.jacoco.core/src/org/jacoco/core/internal/instr/InstrSupport.java
@@ -27,7 +27,7 @@ public final class InstrSupport {
 	}
 
 	/** ASM API version */
-	public static final int ASM_API_VERSION = Opcodes.ASM7;
+	public static final int ASM_API_VERSION = Opcodes.ASM8;
 
 	// === Data Field ===
 
