c541e9ac5008fd8bc432c07d5783ed92590cba60
Julian Centurion <julian.centurion@mercadolibre.com>
2020-12-17 15:54:58 -0300

    Create filter for kotlin 1.4

---
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/KotlinUnsafeCastOperatorFilter1_4.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2020 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.internal.analysis.filter;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+/**
+ * Filters branch in bytecode that Kotlin compiler generates for "unsafe" cast
+ * operator for Kotlin 1.4
+ */
+public class KotlinUnsafeCastOperatorFilter1_4 implements IFilter {
+
+	private static final String KOTLIN_1_4_TYPE_CAST_EXCEPTION = "java/lang/NullPointerException";
+
+	@Override
+	public void filter(MethodNode methodNode, IFilterContext context,
+			IFilterOutput output) {
+		final KotlinUnsafeCastOperatorFilter1_4.Matcher matcher = new KotlinUnsafeCastOperatorFilter1_4.Matcher();
+		for (final AbstractInsnNode i : methodNode.instructions) {
+			matcher.match(i, output);
+		}
+	}
+
+	private static class Matcher extends AbstractMatcher {
+		public void match(final AbstractInsnNode start,
+				final IFilterOutput output) {
+
+			if (Opcodes.IFNONNULL != start.getOpcode()) {
+				return;
+			}
+			cursor = start;
+
+			nextIsType(Opcodes.NEW, KOTLIN_1_4_TYPE_CAST_EXCEPTION);
+			nextIs(Opcodes.DUP);
+			nextIs(Opcodes.LDC);
+			if (cursor == null) {
+				return;
+			}
+			final LdcInsnNode ldc = (LdcInsnNode) cursor;
+			if (!(ldc.cst instanceof String && ((String) ldc.cst)
+					.startsWith("null cannot be cast to non-null type"))) {
+				return;
+			}
+			nextIsInvoke(Opcodes.INVOKESPECIAL, KOTLIN_1_4_TYPE_CAST_EXCEPTION,
+					"<init>", "(Ljava/lang/String;)V");
+			nextIs(Opcodes.ATHROW);
+			if (cursor == null) {
+				return;
+			}
+			if (cursor.getNext() != ((JumpInsnNode) start).label) {
+				return;
+			}
+
+			output.ignore(start, cursor);
+		}
+	}
+}
