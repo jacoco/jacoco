6696abffb1679213dca68a1167ef3e54ec9beb1d
rayduan <duanrui1991>
2021-03-08 11:00:30 +0800

    【modify】更新对象格式

--- org.jacoco.core/src/org/jacoco/core/internal/diff/MethodInfoDto.java
+++ org.jacoco.core/src/org/jacoco/core/internal/diff/MethodInfoDto.java
@@ -16,10 +16,7 @@ package org.jacoco.core.internal.diff;
  * @author dr
  */
 public class MethodInfoDto {
-	/**
-	 * 方法的md5
-	 */
-	public String md5;
+
 	/**
 	 * 方法名
 	 */
@@ -29,13 +26,6 @@ public class MethodInfoDto {
 	 */
 	public String parameters;
 
-	public String getMd5() {
-		return md5;
-	}
-
-	public void setMd5(String md5) {
-		this.md5 = md5;
-	}
 
 	public String getMethodName() {
 		return methodName;
