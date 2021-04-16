f632c2177ab4fccbef8ab50998e301e2650fe672
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-04-16 10:46:06 +0200

    WIP: Add tests

---
+++ jacoco-maven-plugin.test/it/it-report-select-formats/src/test/java/ExampleTest.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2021 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *
+ *******************************************************************************/
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		new Example();
+	}
+
+}
