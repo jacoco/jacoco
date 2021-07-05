7a11b6b87238bf133d891d72f1d7071550c7443a
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-07-05 23:41:38 +0200

    Consolidate place to check current JDK version

--- org.jacoco.core.test.validation.java7/src/org/jacoco/core/test/validation/java7/TryWithResourcesTest.java
+++ org.jacoco.core.test.validation.java7/src/org/jacoco/core/test/validation/java7/TryWithResourcesTest.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.java7;
 
+import org.jacoco.core.test.validation.JavaVersion;
 import org.jacoco.core.test.validation.Source.Line;
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java7.targets.TryWithResourcesTarget;
@@ -28,7 +29,7 @@ public class TryWithResourcesTest extends ValidationTestBase {
 
 	public void assertTry(final Line line) {
 		// without filter this line is covered partly:
-		if (!isJDKCompiler || JAVA_VERSION.isBefore("11")) {
+		if (!isJDKCompiler || JavaVersion.current().isBefore("11")) {
 			assertFullyCovered(line);
 		} else {
 			assertEmpty(line);
@@ -40,7 +41,7 @@ public class TryWithResourcesTest extends ValidationTestBase {
 		if (isJDKCompiler) {
 			// https://bugs.openjdk.java.net/browse/JDK-8134759
 			// javac 7 and 8 up to 8u92 are affected
-			if (JAVA_VERSION.isBefore("1.8.0_92")) {
+			if (JavaVersion.current().isBefore("1.8.0_92")) {
 				assertFullyCovered(line);
 			} else {
 				assertEmpty(line);
@@ -61,9 +62,9 @@ public class TryWithResourcesTest extends ValidationTestBase {
 	public void assertEmptyClose(final Line line) {
 		if (!isJDKCompiler) {
 			assertPartlyCovered(line, 7, 1);
-		} else if (JAVA_VERSION.isBefore("8")) {
+		} else if (JavaVersion.current().isBefore("8")) {
 			assertPartlyCovered(line, 6, 2);
-		} else if (JAVA_VERSION.isBefore("9")) {
+		} else if (JavaVersion.current().isBefore("9")) {
 			assertPartlyCovered(line, 2, 2);
 		} else {
 			assertFullyCovered(line);
@@ -74,9 +75,9 @@ public class TryWithResourcesTest extends ValidationTestBase {
 		// not filtered
 		if (!isJDKCompiler) {
 			assertNotCovered(line, 6, 0);
-		} else if (JAVA_VERSION.isBefore("9")) {
+		} else if (JavaVersion.current().isBefore("9")) {
 			assertNotCovered(line, 4, 0);
-		} else if (JAVA_VERSION.isBefore("11")) {
+		} else if (JavaVersion.current().isBefore("11")) {
 			assertNotCovered(line);
 		} else {
 			assertEmpty(line);
