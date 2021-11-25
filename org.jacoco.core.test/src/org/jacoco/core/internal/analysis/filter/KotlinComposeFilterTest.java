a3a1ba47b9579dfae0b07f76d8e9122c140a6893
lukasz.suski <lukasz.suski@finanteq.com>
2021-11-25 16:19:54 +0100

    Add license info to KotlinComposeFilterTest

--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinComposeFilterTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinComposeFilterTest.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2021 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Łukasz Suski - initial API and implementation
+ *
+ *******************************************************************************/
 package org.jacoco.core.internal.analysis.filter;
 
 import org.jacoco.core.internal.instr.InstrSupport;
