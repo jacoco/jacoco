ac756b60c029d0acf5e1202c9966b2c5a79b6b8c
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2021-12-14 11:15:09 +0100

    Add experimental support for Java 19 class files (#1264)

--- org.jacoco.core.test/src/org/jacoco/core/internal/instr/InstrSupportTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/instr/InstrSupportTest.java
@@ -43,8 +43,8 @@ public class InstrSupportTest {
 	}
 
 	@Test
-	public void classReaderFor_should_read_java_18_class() {
-		final byte[] bytes = createJava18Class();
+	public void classReaderFor_should_read_java_19_class() {
+		final byte[] bytes = createJava19Class();
 
 		final ClassReader classReader = InstrSupport.classReaderFor(bytes);
 
@@ -53,16 +53,16 @@ public class InstrSupportTest {
 			public void visit(final int version, final int access,
 					final String name, final String signature,
 					final String superName, final String[] interfaces) {
-				assertEquals(Opcodes.V17 + 1, version);
+				assertEquals(Opcodes.V18 + 1, version);
 			}
 		}, 0);
 
-		assertArrayEquals(createJava18Class(), bytes);
+		assertArrayEquals(createJava19Class(), bytes);
 	}
 
-	private static byte[] createJava18Class() {
+	private static byte[] createJava19Class() {
 		final ClassWriter cw = new ClassWriter(0);
-		cw.visit(Opcodes.V17 + 1, 0, "Foo", null, "java/lang/Object", null);
+		cw.visit(Opcodes.V18 + 1, 0, "Foo", null, "java/lang/Object", null);
 		cw.visitEnd();
 		return cw.toByteArray();
 	}
@@ -129,7 +129,8 @@ public class InstrSupportTest {
 		assertTrue(InstrSupport.needsFrames(Opcodes.V15));
 		assertTrue(InstrSupport.needsFrames(Opcodes.V16));
 		assertTrue(InstrSupport.needsFrames(Opcodes.V17));
-		assertTrue(InstrSupport.needsFrames(Opcodes.V17 + 1));
+		assertTrue(InstrSupport.needsFrames(Opcodes.V18));
+		assertTrue(InstrSupport.needsFrames(Opcodes.V18 + 1));
 
 		assertTrue(InstrSupport.needsFrames(0x0100));
 	}
