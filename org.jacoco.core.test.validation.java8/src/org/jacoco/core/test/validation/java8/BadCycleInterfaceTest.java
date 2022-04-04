aa16a7c25cfc119ba3486bb114d3d54dc1bcca6c
Marc R. Hoffmann <hoffmann@mountainminds.com>
2022-04-04 23:52:22 +0200

    Improve support for multiple JaCoCo runtimes in the same VM (#1057)

--- org.jacoco.core.test.validation.java8/src/org/jacoco/core/test/validation/java8/BadCycleInterfaceTest.java
+++ org.jacoco.core.test.validation.java8/src/org/jacoco/core/test/validation/java8/BadCycleInterfaceTest.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.java8;
 
+import org.jacoco.core.test.validation.JavaVersion;
 import org.jacoco.core.test.validation.Source.Line;
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java8.targets.BadCycleInterfaceTarget;
@@ -28,7 +29,7 @@ public class BadCycleInterfaceTest extends ValidationTestBase {
 
 	@Test
 	public void method_execution_sequence() throws Exception {
-		if (JAVA_VERSION.isBefore("1.8.0_152")) {
+		if (JavaVersion.current().isBefore("1.8.0_152")) {
 			assertLogEvents("baseclinit", "childdefaultmethod", "childclinit",
 					"childstaticmethod");
 		} else {
@@ -37,7 +38,7 @@ public class BadCycleInterfaceTest extends ValidationTestBase {
 	}
 
 	public void assertBaseClInit(final Line line) {
-		if (JAVA_VERSION.isBefore("1.8.0_152")) {
+		if (JavaVersion.current().isBefore("1.8.0_152")) {
 			// Incorrect interpetation of JVMS 5.5 in JDK 8 causes a default
 			// method to be called before the static initializer of an interface
 			// (see JDK-8098557 and JDK-8164302):
@@ -51,7 +52,7 @@ public class BadCycleInterfaceTest extends ValidationTestBase {
 	}
 
 	public void assertChildDefault(final Line line) throws Exception {
-		if (JAVA_VERSION.isBefore("1.8.0_152")) {
+		if (JavaVersion.current().isBefore("1.8.0_152")) {
 			// Incorrect interpetation of JVMS 5.5 in JDK 8 causes a default
 			// method to be called before the static initializer of an interface
 			// (see JDK-8098557 and JDK-8164302):
