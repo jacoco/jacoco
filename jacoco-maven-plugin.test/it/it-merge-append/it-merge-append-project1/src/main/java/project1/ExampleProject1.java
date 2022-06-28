36cad2ed52d32158f5303c1fc480bfb08ebe989c
muminc <mumin@talk21.com>
2022-06-29 00:16:57 +0100

    Add integration which verifies the results of append by doing a coverage check using the appended file (which should contain coverage data from all projects)

---
+++ jacoco-maven-plugin.test/it/it-merge-append/it-merge-append-project1/src/main/java/project1/ExampleProject1.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2022 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Mads Mohr Christensen - implementation of MergeMojo
+ *
+ *******************************************************************************/
+package project1;
+
+public class ExampleProject1 {
+
+	// coverage provided by project1
+	public void sayHello() {
+		System.out.println("Hello");
+	}
+
+	// coverage provided by project2
+	public void sayHi() {
+		System.out.println("Hi");
+	}
+
+	// coverage provided by project3
+	public void sayHowdy() {
+		System.out.println("Howdy");
+	}
+
+}
---
+++ jacoco-maven-plugin.test/it/it-merge-append/it-merge-append-project2/src/test/java/project2/ExampleProject2Test.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2022 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Mads Mohr Christensen - implementation of MergeMojo
+ *
+ *******************************************************************************/
+package project2;
+
+import project1.ExampleProject1;
+
+import org.junit.Test;
+
+public class ExampleProject2Test {
+
+	@Test
+	public void test() {
+		new ExampleProject2().sayHello();
+	}
+
+	@Test
+	public void partiallyTestProject1(){
+		new ExampleProject1().sayHi();
+	}
+
+}
