c561c13f07a544a3a8cd4430c2bb8eae7deea229
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2023-03-31 17:31:30 +0200

    Fix validation test for Java 21 (#1422)

--- org.jacoco.core.test.validation.java20/src/org/jacoco/core/test/validation/java20/RecordPatternsTest.java
+++ org.jacoco.core.test.validation.java20/src/org/jacoco/core/test/validation/java20/RecordPatternsTest.java
@@ -12,6 +12,8 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.java20;
 
+import org.jacoco.core.test.validation.JavaVersion;
+import org.jacoco.core.test.validation.Source.Line;
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java20.targets.RecordPatternsTarget;
 
@@ -22,6 +24,24 @@ public class RecordPatternsTest extends ValidationTestBase {
 
 	public RecordPatternsTest() {
 		super(RecordPatternsTarget.class);
+	}
+
+	public void assertSwitchStatementCase(Line line) {
+		if (JavaVersion.current().isBefore("21")) {
+			assertFullyCovered(line);
+		} else {
+			// https://github.com/openjdk/jdk/commit/138cdc9283ae8f3367e51f0fe7e27833118dd7cb
+			assertPartlyCovered(line);
+		}
+	}
+
+	public void assertSwitchStatementDefault(Line line) {
+		if (JavaVersion.current().isBefore("21")) {
+			assertPartlyCovered(line);
+		} else {
+			// https://github.com/openjdk/jdk/commit/138cdc9283ae8f3367e51f0fe7e27833118dd7cb
+			assertFullyCovered(line);
+		}
 	}
 
 }
