bc7192197316966dcf7807e523bec2b95b78a855
Evgeny Mandrikov <mandrikov@gmail.com>
2023-06-05 16:32:09 +0200

    (WIP) add filter

--- org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/RecordPatternFilter.java
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/RecordPatternFilter.java
@@ -12,13 +12,44 @@
  *******************************************************************************/
 package org.jacoco.core.internal.analysis.filter;
 
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TryCatchBlockNode;
 
+/**
+ * Filters code that is generated for record patterns.
+ */
 final class RecordPatternFilter implements IFilter {
 
 	public void filter(final MethodNode methodNode,
 			final IFilterContext context, final IFilterOutput output) {
-		// TODO
+		final Matcher matcher = new Matcher();
+		for (final TryCatchBlockNode t : methodNode.tryCatchBlocks) {
+			if ("java/lang/Throwable".equals(t.type)) {
+				matcher.match(t.handler, output);
+			}
+		}
+	}
+
+	private static class Matcher extends AbstractMatcher {
+		void match(final AbstractInsnNode start, final IFilterOutput output) {
+			cursor = start;
+			nextIsVar(Opcodes.ASTORE, "cause");
+			nextIsType(org.objectweb.asm.Opcodes.NEW,
+					"java/lang/MatchException");
+			nextIs(Opcodes.DUP);
+			nextIsVar(Opcodes.ALOAD, "cause");
+			nextIsInvoke(Opcodes.INVOKEVIRTUAL, "java/lang/Throwable",
+					"toString", "()Ljava/lang/String;");
+			nextIsVar(Opcodes.ALOAD, "cause");
+			nextIsInvoke(Opcodes.INVOKESPECIAL, "java/lang/MatchException",
+					"<init>", "(Ljava/lang/String;Ljava/lang/Throwable;)V");
+			nextIs(Opcodes.ATHROW);
+			if (cursor != null) {
+				output.ignore(start, cursor);
+			}
+		}
 	}
 
 }
