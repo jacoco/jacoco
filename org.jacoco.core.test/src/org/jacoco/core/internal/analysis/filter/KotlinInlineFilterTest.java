853bd56146e8c78fccdf3273176b701856fda6a1
Evgeny Mandrikov <mandrikov@gmail.com>
2023-10-13 00:19:35 +0200

    (WIP) add unit test

--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinInlineFilterTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinInlineFilterTest.java
@@ -22,6 +22,7 @@ import org.jacoco.core.internal.instr.InstrSupport;
 import org.junit.Test;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 /**
@@ -259,6 +260,69 @@ public class KotlinInlineFilterTest extends FilterTestBase {
 		shouldIgnorePrevious(m);
 		m.visitLineNumber(5, label2);
 		m.visitInsn(Opcodes.RETURN);
+
+		filter.filter(m, context, output);
+
+		assertIgnored(expectedRanges.toArray(new Range[0]));
+	}
+
+	/**
+	 * <pre>
+	 * inline fun example(crossinline lambda: () -> Unit): () -> Unit {
+	 *   return {
+	 *     lambda()
+	 *   }
+	 * }
+	 *
+	 * fun callsite() {
+	 *   example {
+	 *   }()
+	 * }
+	 * </pre>
+	 */
+	@Test
+	public void should_filter_all_lines() {
+		context.className = "ExampleKt$callsite$$inlined$example$1";
+		context.sourceFileName = "Example.kt";
+		context.sourceDebugExtension = "" //
+				+ "SMAP\n" //
+				+ "Example.kt\n" // OutputFileName=Example.kt
+				+ "Kotlin\n" // DefaultStratumId=Kotlin
+				+ "*S Kotlin\n" // StratumID=Kotlin
+				+ "*F\n" // FileSection
+				+ "+ 1 Example.kt\n" // FileID=1
+				+ "ExampleKt$example$1\n" //
+				+ "+ 2 Example.kt\n" // FileID=2
+				+ "ExampleKt\n" //
+				+ "*L\n" // LineSection
+				+ "1#1,11:1\n" // InputStartLine=1,LineFileID=1,RepeatCount=11,OutputStartLine=1
+				+ "9#2:12\n" // InputStartLine=9,LineFileID=2,OutputStartLine=12
+				+ "*E\n"; // EndSection
+		context.classAnnotations
+				.add(KotlinGeneratedFilter.KOTLIN_METADATA_DESC);
+
+		Label label0 = new Label();
+		m.visitLabel(label0);
+		m.visitLineNumber(3, label0);
+		m.visitInsn(Opcodes.ICONST_0);
+		m.visitVarInsn(Opcodes.ISTORE, 1);
+		Label label1 = new Label();
+		m.visitLabel(label1);
+		m.visitLineNumber(12, label1);
+		m.visitInsn(Opcodes.NOP);
+		Label label2 = new Label();
+		m.visitLabel(label2);
+		m.visitLineNumber(3, label2);
+		m.visitInsn(Opcodes.NOP);
+		Label label3 = new Label();
+		m.visitLabel(label3);
+		m.visitLineNumber(4, label3);
+		m.visitInsn(Opcodes.RETURN);
+
+		for (AbstractInsnNode i = m.instructions.getFirst()
+				.getNext(); i != null; i = i.getNext()) {
+			expectedRanges.add(new Range(i, i));
+		}
 
 		filter.filter(m, context, output);
 
