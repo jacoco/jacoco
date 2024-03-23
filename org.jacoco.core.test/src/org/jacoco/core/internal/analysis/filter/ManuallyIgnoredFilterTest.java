1f961bfc836a7ba554ff4d8eb782589858919208
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2024-03-23 08:38:06 +0100

    add ManuallyIngoredFilter

--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/ManuallyIgnoredFilterTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/ManuallyIgnoredFilterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2023 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
@@ -7,7 +7,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *    Evgeny Mandrikov - initial API and implementation
+ *    Orestis Gkorgkas - initial API and implementation
  *
  *******************************************************************************/
 package org.jacoco.core.internal.analysis.filter;
@@ -22,65 +22,65 @@ import org.objectweb.asm.tree.MethodNode;
  */
 public class ManuallyIgnoredFilterTest extends FilterTestBase {
 
-  public static final String SOME_METHOD_NAME = "someMethodName";
-  private static final String ANNOTATION_VALUE = "L" + ManuallyIgnoredFilter.ANNOTATION_VALUE + ";";
-  private final IFilter filter = new ManuallyIgnoredFilter();
+	public static final String SOME_METHOD_NAME = "someMethodName";
+	private static final String ANNOTATION_VALUE = "L"
+			+ ManuallyIgnoredFilter.ANNOTATION_VALUE + ";";
+	private final IFilter filter = new ManuallyIgnoredFilter();
 
-  @Test
-  public void should_filter_methods_annotated_with_runtime_invisible_annotation_JacocoIgnored() {
-    final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
-      SOME_METHOD_NAME, "()I", null, null);
-    m.visitAnnotation(ANNOTATION_VALUE, true);
+	@Test
+	public void should_filter_methods_annotated_with_runtime_invisible_annotation_JacocoIgnored() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				SOME_METHOD_NAME, "()I", null, null);
+		m.visitAnnotation(ANNOTATION_VALUE, true);
 
-    m.visitInsn(Opcodes.ICONST_0);
-    m.visitInsn(Opcodes.IRETURN);
+		m.visitInsn(Opcodes.ICONST_0);
+		m.visitInsn(Opcodes.IRETURN);
 
-    filter.filter(m, context, output);
+		filter.filter(m, context, output);
 
-    assertMethodIgnored(m);
-  }
+		assertMethodIgnored(m);
+	}
 
-  @Test
-  public void should_filter_classes_annotated_with_runtime_visible_annotation_JacocoIgnored() {
-    final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
-      "readExternal", "()V", null, null);
+	@Test
+	public void should_filter_classes_annotated_with_runtime_visible_annotation_JacocoIgnored() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"readExternal", "()V", null, null);
 
-    m.visitInsn(Opcodes.NOP);
-    context.classAnnotations.add(ANNOTATION_VALUE);
+		m.visitInsn(Opcodes.NOP);
+		context.classAnnotations.add(ANNOTATION_VALUE);
 
-    filter.filter(m, context, output);
+		filter.filter(m, context, output);
 
-    assertMethodIgnored(m);
-  }
+		assertMethodIgnored(m);
+	}
 
+	@Test
+	public void should_not_filter_when_no_annotations() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"hashCode", "()I", null, null);
 
-  @Test
-  public void should_not_filter_when_no_annotations() {
-    final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
-      "hashCode", "()I", null, null);
+		m.visitInsn(Opcodes.ICONST_0);
+		m.visitInsn(Opcodes.IRETURN);
 
-    m.visitInsn(Opcodes.ICONST_0);
-    m.visitInsn(Opcodes.IRETURN);
+		filter.filter(m, context, output);
 
-    filter.filter(m, context, output);
+		assertIgnored();
+	}
 
-    assertIgnored();
-  }
+	@Test
+	public void should_not_filter_when_other_annotations() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"hashCode", "()I", null, null);
+		m.visitAnnotation("LOtherAnnotation;", true);
 
-  @Test
-  public void should_not_filter_when_other_annotations() {
-    final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
-      "hashCode", "()I", null, null);
-    m.visitAnnotation("LOtherAnnotation;", true);
+		m.visitInsn(Opcodes.ICONST_0);
+		m.visitInsn(Opcodes.IRETURN);
 
-    m.visitInsn(Opcodes.ICONST_0);
-    m.visitInsn(Opcodes.IRETURN);
+		context.classAnnotations.add("LOtherAnnotation;");
 
-    context.classAnnotations.add("LOtherAnnotation;");
+		filter.filter(m, context, output);
 
-    filter.filter(m, context, output);
-
-    assertIgnored();
-  }
+		assertIgnored();
+	}
 
 }
--- org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/ManuallyIgnoredFilter.java
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/ManuallyIgnoredFilter.java
@@ -1,54 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Orestis Gkorgkas - initial API and implementation
+ *
+ *******************************************************************************/
 package org.jacoco.core.internal.analysis.filter;
 
 import java.util.List;
+
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.MethodNode;
 
 /**
  * Filters out any annotation that is named has the value "JacocoIgnored".
- * Example:
- * The following method that for some reason is not reachable by tests will be ignored by Jacoco:
- * <pre>{@code
- *    @JacocoIgnored
- *    public void foo(Object bar){
- *      //DO SOMETHING HERE
- *    }
+ * Example: The following method that for some reason is not reachable by tests
+ * will be ignored by Jacoco:
+ *
+ * <pre>{@code @JacocoIgnored
+ * public void foo(Object bar) {
+ * 	// DO SOMETHING HERE
+ * }
  *
  * }</pre>
  */
 public class ManuallyIgnoredFilter implements IFilter {
 
-  public static final String ANNOTATION_VALUE = "JacocoIgnored";
+	public static final String ANNOTATION_VALUE = "JacocoIgnored";
 
-  @Override
-  public void filter(MethodNode methodNode, IFilterContext context, IFilterOutput output) {
-    for (String annotation : context.getClassAnnotations()) {
-      if (matches(annotation)) {
-        output.ignore(methodNode.instructions.getFirst(),
-          methodNode.instructions.getLast());
-        return;
-      }
-    }
+	@Override
+	public void filter(MethodNode methodNode, IFilterContext context,
+			IFilterOutput output) {
+		for (String annotation : context.getClassAnnotations()) {
+			if (matches(annotation)) {
+				output.ignore(methodNode.instructions.getFirst(),
+						methodNode.instructions.getLast());
+				return;
+			}
+		}
 
-    if (presentIn(methodNode.visibleAnnotations)) {
-      output.ignore(methodNode.instructions.getFirst(),
-        methodNode.instructions.getLast());
-    }
+		if (presentIn(methodNode.visibleAnnotations)) {
+			output.ignore(methodNode.instructions.getFirst(),
+					methodNode.instructions.getLast());
+		}
 
-  }
+	}
 
-  private static boolean matches(final String annotation) {
-    return annotation.contains(ANNOTATION_VALUE);
-  }
+	private static boolean matches(final String annotation) {
+		return annotation.contains(ANNOTATION_VALUE);
+	}
 
-  private static boolean presentIn(final List<AnnotationNode> annotations) {
-    if (annotations != null) {
-      for (AnnotationNode annotation : annotations) {
-        if (matches(annotation.desc)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
+	private static boolean presentIn(final List<AnnotationNode> annotations) {
+		if (annotations != null) {
+			for (AnnotationNode annotation : annotations) {
+				if (matches(annotation.desc)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
