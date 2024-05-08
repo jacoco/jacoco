63ada1f4471b32232f4cbdfbcd4b648793a26786
Evgeny Mandrikov <mandrikov@gmail.com>
2024-05-08 23:21:46 +0200

    (WIP) add unit tests

--- org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/Filters.java
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/Filters.java
@@ -47,6 +47,7 @@ public final class Filters implements IFilter {
 				new KotlinWhenStringFilter(),
 				new KotlinUnsafeCastOperatorFilter(),
 				new KotlinNotNullOperatorFilter(),
+				new KotlinInlineClassFilter(),
 				new KotlinDefaultArgumentsFilter(), new KotlinInlineFilter(),
 				new KotlinCoroutineFilter(), new KotlinDefaultMethodsFilter(),
 				new KotlinComposeFilter());
