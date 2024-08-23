bf106b152c3af3f5f1df1848c0d378f829879273
Olivier Lamy <olamy@apache.org>
2024-08-23 15:57:41 +1000

    Add new Mojo to aggregate from aggregators, no need to add any dependency
    Signed-off-by: Olivier Lamy <olamy@apache.org>

---
+++ jacoco-maven-plugin.test/it/it-report-aggregate-all/child2v2/src/test/java/package2/Example2Test.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2025 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Marc R. Hoffmann, Jan Wloka - initial API and implementation
+ *
+ *******************************************************************************/
+package package2;
+
+import org.junit.Test;
+
+public class Example2Test {
+
+	@Test
+	public void test() {
+		new Example2().a();
+	}
+
+}
