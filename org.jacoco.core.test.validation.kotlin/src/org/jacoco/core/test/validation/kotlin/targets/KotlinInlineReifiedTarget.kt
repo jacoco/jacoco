1707be268d60346c236334bf4d33bb46ca6e749b
Evgeny Mandrikov <mandrikov@gmail.com>
2024-09-08 13:04:19 +0200

    (WIP) add validation tests

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinInlineReifiedTarget.kt
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+import org.jacoco.core.test.validation.targets.Stubs.nop
+
+/**
+ * Test target containing `inline` function with
+ * [`reified` type parameter](https://kotlinlang.org/docs/inline-functions.html#reified-type-parameters).
+ */
+object KotlinInlineReifiedTarget {
+
+    private inline fun <reified T> example() { // assertEmpty()
+        nop(T::class) // assertFullyCovered()
+    } // assertFullyCovered()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        example<String>()
+    }
+
+}
