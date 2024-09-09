e951757eb7682948607277cc1cfe0f6ea7fc8caa
Evgeny Mandrikov <mandrikov@gmail.com>
2024-09-09 15:23:53 +0200

    (WIP) improve validation tests

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinInlineReifiedTest.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinInlineReifiedTest.java
@@ -12,8 +12,15 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.kotlin;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashSet;
+
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.kotlin.targets.KotlinInlineReifiedTarget;
+import org.junit.Test;
 
 /**
  * Test of code coverage in {@link KotlinInlineReifiedTarget}.
@@ -22,6 +29,24 @@ public class KotlinInlineReifiedTest extends ValidationTestBase {
 
 	public KotlinInlineReifiedTest() {
 		super(KotlinInlineReifiedTarget.class);
+	}
+
+	@Test
+	public void compiler_should_generate_synthetic_method() {
+		final HashSet<String> names = new HashSet<String>();
+		for (final Method method : KotlinInlineReifiedTarget.class
+				.getDeclaredMethods()) {
+			if (method.isSynthetic()) {
+				names.add(method.getName());
+			}
+		}
+
+		assertEquals(Collections.singleton("example"), names);
+	}
+
+	@Test
+	public void test_method_count() {
+		assertMethodCount(2);
 	}
 
 }
--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinJvmSyntheticTest.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinJvmSyntheticTest.java
@@ -12,8 +12,15 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.kotlin;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashSet;
+
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.kotlin.targets.KotlinJvmSyntheticTarget;
+import org.junit.Test;
 
 /**
  * Test of code coverage in {@link KotlinJvmSyntheticTarget}.
@@ -22,6 +29,24 @@ public class KotlinJvmSyntheticTest extends ValidationTestBase {
 
 	public KotlinJvmSyntheticTest() {
 		super(KotlinJvmSyntheticTarget.class);
+	}
+
+	@Test
+	public void compiler_should_generate_synthetic_method() {
+		final HashSet<String> names = new HashSet<String>();
+		for (final Method method : KotlinJvmSyntheticTarget.class
+				.getDeclaredMethods()) {
+			if (method.isSynthetic()) {
+				names.add(method.getName());
+			}
+		}
+
+		assertEquals(Collections.singleton("example"), names);
+	}
+
+	@Test
+	public void test_method_count() {
+		assertMethodCount(2);
 	}
 
 }
