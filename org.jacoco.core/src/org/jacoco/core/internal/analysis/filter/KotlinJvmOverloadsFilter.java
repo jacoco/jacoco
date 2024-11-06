58a38ceac974a966eb489ce938d87c3d4a0a63ff
Evgeny Mandrikov <mandrikov@gmail.com>
2024-11-07 00:15:57 +0100

    (WIP) add unit tests

---
+++ org.jacoco.core/src/org/jacoco/core/internal/analysis/filter/KotlinJvmOverloadsFilter.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2024 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.internal.analysis.filter;
+
+import org.objectweb.asm.tree.MethodNode;
+
+/**
+ * Filters methods that Kotlin compiler generates for functions and constructors
+ * annotated with {@code JvmOverloads}.
+ */
+final class KotlinJvmOverloadsFilter implements IFilter {
+
+	public void filter(final MethodNode methodNode,
+			final IFilterContext context, final IFilterOutput output) {
+		// TODO
+	}
+
+}
