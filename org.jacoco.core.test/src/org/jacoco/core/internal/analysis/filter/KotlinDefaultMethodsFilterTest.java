fb723693fbe0c6ad99bb531d896a5466df2e2a67
Evgeny Mandrikov <mandrikov@gmail.com>
2025-10-28 22:10:31 +0100

    (WIP) add unit tests

--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinDefaultMethodsFilterTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/KotlinDefaultMethodsFilterTest.java
@@ -69,4 +69,73 @@ public class KotlinDefaultMethodsFilterTest extends FilterTestBase {
 		assertIgnored(m);
 	}
 
+	/**
+	 * <pre>
+	 * interface Example {
+	 *     fun example(s: String) = s
+	 * }
+	 * </pre>
+	 *
+	 * with compiler option <code>-jvm-default=enable</code> which <a href=
+	 * "https://github.com/JetBrains/kotlin/commit/4e2914ac5997136bcc8c58b76c8dbef7e2dbe5c0">
+	 * became default starting from Kotlin 2.2.0</a>
+	 *
+	 * @see #should_not_filter_non_compatibility_methods()
+	 */
+	@Test
+	public void should_filter_compatibility_methods() {
+		context.className = "Example$DefaultImpls";
+		final MethodNode m = new MethodNode(
+				Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC
+						| Opcodes.ACC_DEPRECATED,
+				"example", "(LExample;Ljava/lang/String;)Ljava/lang/String;",
+				null, null);
+		m.visitVarInsn(Opcodes.ALOAD, 1);
+		m.visitLdcInsn("s");
+		m.visitMethodInsn(Opcodes.INVOKESTATIC,
+				"kotlin/jvm/internal/Intrinsics", "checkNotNullParameter",
+				"(Ljava/lang/Object;Ljava/lang/String;)V", false);
+		m.visitVarInsn(Opcodes.ALOAD, 0);
+		m.visitVarInsn(Opcodes.ALOAD, 1);
+		m.visitMethodInsn(Opcodes.INVOKESTATIC, "Example", "access$example$jd",
+				"(LExample;Ljava/lang/String;)Ljava/lang/String;", true);
+		m.visitInsn(Opcodes.ARETURN);
+
+		filter.filter(m, context, output);
+
+		assertMethodIgnored(m);
+	}
+
+	/**
+	 * <pre>
+	 * interface Example {
+	 *     fun example(s: String) = s
+	 * }
+	 * </pre>
+	 *
+	 * with compiler option <code>-jvm-default=disable</code> which <a href=
+	 * "https://github.com/JetBrains/kotlin/commit/4e2914ac5997136bcc8c58b76c8dbef7e2dbe5c0">
+	 * was default prior to Kotlin 2.2.0</a>
+	 *
+	 * @see #should_filter_compatibility_methods()
+	 */
+	@Test
+	public void should_not_filter_non_compatibility_methods() {
+		context.className = "Example$DefaultImpls";
+		final MethodNode m = new MethodNode(
+				Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "example",
+				"(LExample;Ljava/lang/String;)Ljava/lang/String;", null, null);
+		m.visitVarInsn(Opcodes.ALOAD, 1);
+		m.visitLdcInsn("s");
+		m.visitMethodInsn(Opcodes.INVOKESTATIC,
+				"kotlin/jvm/internal/Intrinsics", "checkNotNullParameter",
+				"(Ljava/lang/Object;Ljava/lang/String;)V", false);
+		m.visitVarInsn(Opcodes.ALOAD, 1);
+		m.visitInsn(Opcodes.ARETURN);
+
+		filter.filter(m, context, output);
+
+		assertIgnored(m);
+	}
+
 }
