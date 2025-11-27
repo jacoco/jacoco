2ec49cedac244e09e93183075919b6fd7e265e21
Evgeny Mandrikov <mandrikov@gmail.com>
2025-11-27 14:44:58 +0100

    Update validation test for Java 21 JEP 441: null in switch

--- org.jacoco.core.test.validation.java21/src/org/jacoco/core/test/validation/java21/targets/SwitchPatternMatchingTarget.java
+++ org.jacoco.core.test.validation.java21/src/org/jacoco/core/test/validation/java21/targets/SwitchPatternMatchingTarget.java
@@ -14,13 +14,15 @@ package org.jacoco.core.test.validation.java21.targets;
 
 import static org.jacoco.core.test.validation.targets.Stubs.nop;
 
+import org.jacoco.core.test.validation.targets.Stubs;
+
 /**
  * This target exercises pattern matching for switch
  * (<a href="https://openjdk.org/jeps/441">JEP 441</a>).
  */
 public class SwitchPatternMatchingTarget {
 
-	private static void example(Object o) {
+	private static void guard(Object o) {
 		switch (o) { // assertFullyCovered(1, 2)
 		case String s when s.length() == 0 -> // assertFullyCovered(0, 2)
 			nop(s); // assertFullyCovered()
@@ -31,9 +33,66 @@ public class SwitchPatternMatchingTarget {
 		}
 	}
 
+	private static void nullCase(Object o) {
+		switch (o) { // assertFullyCovered(0, 3)
+		case String s -> // assertFullyCovered()
+			nop(s); // assertFullyCovered()
+		case null -> // assertEmpty()
+			nop("null"); // assertFullyCovered()
+		default -> // assertEmpty()
+			nop("default"); // assertFullyCovered()
+		} // assertEmpty()
+	}
+
+	private static void nullDefault(Object o) {
+		switch (o) { // assertFullyCovered(0, 2)
+		case String s -> // assertFullyCovered()
+			nop(s); // assertFullyCovered()
+		case null, default -> // assertEmpty()
+			nop("default"); // assertFullyCovered()
+		} // assertEmpty()
+	}
+
+	private static void enumSwitch(Stubs.Enum e) {
+		switch (e) { // assertFullyCovered(0, 3)
+		case null -> // assertEmpty()
+			nop("null"); // assertFullyCovered()
+		case A, C -> // assertEmpty()
+			nop("A, C"); // assertFullyCovered()
+		case B -> // assertEmpty()
+			nop("B"); // assertFullyCovered()
+		} // assertEmpty()
+	}
+
+	private static void stringSwitch(String s) {
+		switch (s) { // assertFullyCovered(3, 1)
+		case null -> // assertEmpty()
+			nop("null"); // assertNotCovered()
+		case "a", "c" -> // assertEmpty()
+			nop("a, c"); // assertFullyCovered()
+		case "b" -> // assertEmpty()
+			nop("b"); // assertNotCovered()
+		default -> // assertEmpty()
+			nop("default"); // assertNotCovered()
+		} // assertEmpty()
+	}
+
 	public static void main(String[] args) {
-		example("");
-		example("a");
+		guard("");
+		guard("a");
+
+		nullCase(null);
+		nullCase("");
+		nullCase(new Object());
+
+		nullDefault(null);
+		nullDefault("");
+
+		enumSwitch(null);
+		enumSwitch(Stubs.Enum.A);
+		enumSwitch(Stubs.Enum.B);
+
+		stringSwitch("a");
 	}
 
 }
