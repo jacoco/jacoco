05dc8d5a8de83f8e381e2e63c41ac1005d51be46
Evgeny Mandrikov <mandrikov@gmail.com>
2025-12-16 22:49:57 +0100

    (WIP) add validation test

---
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/targets/KotlinInlineReflectionTarget.kt
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2025 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.kotlin.targets
+
+import org.jacoco.core.test.validation.targets.Stubs.nop
+
+/**
+ * Test target with `inline` function invoked via reflection.
+ */
+object KotlinInlineReflectionTarget {
+
+    private inline fun example() { // assertEmpty()
+        nop() // assertFullyCovered()
+    } // assertFullyCovered()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        this.javaClass.declaredMethods
+            .single { it.name.equals("example") }
+            .invoke(this)
+    }
+
+}
