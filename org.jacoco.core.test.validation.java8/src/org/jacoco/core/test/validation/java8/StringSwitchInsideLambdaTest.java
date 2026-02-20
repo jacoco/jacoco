532aefbdb9a59aaeb86096c4e22bfa4c18eb23ed
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2026-02-20 13:27:50 +0100

    Improve StringSwitchJavacFilter (#2023)

---
+++ org.jacoco.core.test.validation.java8/src/org/jacoco/core/test/validation/java8/StringSwitchInsideLambdaTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2026 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.java8;
+
+import org.jacoco.core.test.validation.ValidationTestBase;
+import org.jacoco.core.test.validation.java8.targets.StringSwitchInsideLambdaTarget;
+
+/**
+ * Test of code coverage in {@link StringSwitchInsideLambdaTarget}.
+ */
+public class StringSwitchInsideLambdaTest extends ValidationTestBase {
+
+	public StringSwitchInsideLambdaTest() {
+		super(StringSwitchInsideLambdaTarget.class);
+	}
+
+}
