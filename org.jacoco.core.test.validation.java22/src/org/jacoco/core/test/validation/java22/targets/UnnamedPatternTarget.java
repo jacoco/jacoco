bb3947cf68974f36b6afa0d814f539497b79ef23
Evgeny Mandrikov <mandrikov@gmail.com>
2026-06-20 01:03:50 +0200

    Add validation test for Java 22 JEP 456: unnamed patterns

---
+++ org.jacoco.core.test.validation.java22/src/org/jacoco/core/test/validation/java22/targets/UnnamedPatternTarget.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2026 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.test.validation.java22.targets;
+
+import static org.jacoco.core.test.validation.targets.Stubs.nop;
+
+/**
+ * <a href="https://openjdk.org/jeps/456">JEP 456</a>
+ */
+public class UnnamedPatternTarget {
+
+	record R(Object c) {
+	}
+
+	private static String multiplePatternsInCase(Object o) {
+		return switch (o) { // assertFullyCovered(0,2)
+		case R(Float _), R(Double _) -> // assertJavacPartly(3,1) assertEcjPartly(6,2)
+			"case"; // assertFullyCovered()
+		default -> // assertEmpty()
+			"default"; // assertFullyCovered()
+		}; // assertEmpty()
+	}
+
+	private static String multiplePatternsInCaseWithGuard(Object o, int x) {
+		return switch (o) { // assertFullyCovered(0,2)
+		case R(Float _), R(Double _) when x > 0 -> // assertJavacPartly(4,2) assertEcjPartly(7,3)
+			"case"; // assertFullyCovered()
+		default -> // assertEmpty()
+			"default"; // assertFullyCovered()
+		}; // assertEmpty()
+	}
+
+	public static void main(String[] args) {
+		nop(multiplePatternsInCase(new R(1F)));
+		nop(multiplePatternsInCase(""));
+
+		nop(multiplePatternsInCaseWithGuard(new R(1F), 1));
+		nop(multiplePatternsInCaseWithGuard("", 2));
+	}
+
+}
