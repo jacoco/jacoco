4b6a6311e2a5efc0c2a3c8ac1faa59991935a59d
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2026-07-31 00:53:06 +0300

    Add utilities for creating snapshots of method bytecode (#2192)

--- org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
+++ org.jacoco.core.test/src/org/jacoco/core/test/validation/ValidationTestBase.java
@@ -36,6 +36,7 @@ import org.jacoco.core.internal.analysis.CounterImpl;
 import org.jacoco.core.internal.instr.InstrSupport;
 import org.jacoco.core.test.InstrumentingLoader;
 import org.jacoco.core.test.TargetLoader;
+import org.jacoco.core.test.snapshot.MethodSnapshot;
 import org.jacoco.core.test.validation.Source.Line;
 import org.jacoco.core.test.validation.targets.Stubs;
 import org.jacoco.report.DirectorySourceFileLocator;
@@ -147,6 +148,19 @@ public abstract class ValidationTestBase {
 						asmWriter), 0);
 		textWriter.close();
 		asmWriter.close();
+	}
+
+	/**
+	 * {@link MethodSnapshot#assertSnapshot(Class, String, File, File)} with
+	 * predefined file paths.
+	 */
+	protected final void assertSnapshot(final Class<?> targetClass,
+			final String targetMethod, String name) throws Exception {
+		name = targetClass.getSimpleName() + "/" + name;
+		final String expected = "../org.jacoco.core.test/snapshots/" + name;
+		final String actual = "target/snapshots/" + name;
+		MethodSnapshot.assertSnapshot(targetClass, targetMethod,
+				new File(expected), new File(actual));
 	}
 
 	/**
