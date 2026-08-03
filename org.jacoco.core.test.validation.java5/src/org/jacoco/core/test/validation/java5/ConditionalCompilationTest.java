610cf111f0b26c16b76a6856ad413f046edb719c
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-03 10:42:58 +0200

    (WIP) snapshots

--- org.jacoco.core.test.validation.java5/src/org/jacoco/core/test/validation/java5/ConditionalCompilationTest.java
+++ org.jacoco.core.test.validation.java5/src/org/jacoco/core/test/validation/java5/ConditionalCompilationTest.java
@@ -14,6 +14,7 @@ package org.jacoco.core.test.validation.java5;
 
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java5.targets.ConditionalCompilationTarget;
+import org.junit.Test;
 
 /**
  * Test of code coverage in {@link ConditionalCompilationTarget}.
@@ -22,6 +23,14 @@ public class ConditionalCompilationTest extends ValidationTestBase {
 
 	public ConditionalCompilationTest() {
 		super(ConditionalCompilationTarget.class);
+	}
+
+	@Test
+	public void bytecodeSnapshots() throws Exception {
+		assertSnapshot(ConditionalCompilationTarget.class, "conditionFalse",
+				"condition_constant_false.txt");
+		assertSnapshot(ConditionalCompilationTarget.class, "conditionTrue",
+				"condition_constant_true.txt");
 	}
 
 }
