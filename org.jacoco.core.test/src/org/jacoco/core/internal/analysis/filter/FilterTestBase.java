ecf9bfb458425a1bd09a406c5bb845e5a4c1e490
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-05 11:37:17 +0200

    Add `assertSnapshot` to `FilterTestBase`

--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/FilterTestBase.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/FilterTestBase.java
@@ -16,15 +16,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.jacoco.core.test.snapshot.MethodSnapshot;
+import org.jacoco.core.test.snapshot.MethodSnapshotCommentsHandler;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -58,6 +63,81 @@ public abstract class FilterTestBase {
 			actualReplacements.put(source, replacements.values());
 		}
 	};
+
+	final void assertSnapshot(final IFilter filter, final String snapshotPath)
+			throws Exception {
+		final FileReader reader = new FileReader(snapshotPath);
+		assertSnapshot(filter, context, reader);
+		reader.close();
+	}
+
+	static void assertSnapshot(final IFilter filter,
+			final IFilterContext context, final Reader snapshot)
+			throws Exception {
+		final HashMap<String, Range> rangesByName = new HashMap<String, FilterTestBase.Range>();
+		final HashMap<String, ArrayList<Replacement>> replacementsByName = new HashMap<String, ArrayList<Replacement>>();
+		final HashMap<String, AbstractInsnNode> replacedInstructionsByName = new HashMap<String, AbstractInsnNode>();
+		final MethodSnapshotCommentsHandler commentsHandler = new MethodSnapshotCommentsHandler() {
+			final Pattern pattern = Pattern
+					.compile("// previous instruction (?:" //
+							+ "starts ignore range (.++)|" //
+							+ "ends ignore range (.++)|" //
+							+ "replaced by (.++)|" //
+							+ "branch ([0-9]++) creates branch ([0-9]++) for (.++)" //
+							+ ")");
+
+			public void onComment(final String comment,
+					final AbstractInsnNode precedingInstruction) {
+				if (precedingInstruction == null) {
+					return;
+				}
+				final Matcher matcher = pattern.matcher(comment);
+				if (!matcher.matches()) {
+					throw new IllegalStateException(
+							"Invalid syntax: " + comment);
+				}
+				final String group1 = matcher.group(1);
+				final String group2 = matcher.group(2);
+				final String group3 = matcher.group(3);
+				if (group1 != null) {
+					final Range range = new Range();
+					rangesByName.put(group1, range);
+					range.fromInclusive = precedingInstruction;
+				} else if (group2 != null) {
+					final Range range = rangesByName.get(group2);
+					range.toInclusive = precedingInstruction;
+				} else if (group3 != null) {
+					replacedInstructionsByName.put(group3,
+							precedingInstruction);
+				} else {
+					final int instructionBranch = Integer
+							.parseInt(matcher.group(4));
+					final int newBranch = Integer.parseInt(matcher.group(5));
+					final String name = matcher.group(6);
+					ArrayList<Replacement> r = replacementsByName.get(name);
+					if (r == null) {
+						r = new ArrayList<Replacement>();
+						replacementsByName.put(name, r);
+					}
+					r.add(new Replacement(newBranch, precedingInstruction,
+							instructionBranch));
+				}
+			}
+		};
+		final MethodNode methodNode = MethodSnapshot.parse(snapshot,
+				commentsHandler);
+		final FilterTestBase output = new FilterTestBase() {
+		};
+		filter.filter(methodNode, context, output.output);
+		output.assertIgnored(methodNode,
+				rangesByName.values().toArray(new Range[0]));
+		final HashMap<AbstractInsnNode, List<Replacement>> replacements = new HashMap<AbstractInsnNode, List<Replacement>>();
+		for (Map.Entry<String, AbstractInsnNode> e : replacedInstructionsByName
+				.entrySet()) {
+			replacements.put(e.getValue(), replacementsByName.get(e.getKey()));
+		}
+		output.assertReplacedBranches(methodNode, replacements);
+	}
 
 	final void assertIgnored(final MethodNode methodNode,
 			final Range... expected) {
@@ -132,7 +212,8 @@ public abstract class FilterTestBase {
 		}
 		Collections.sort(actualStrings);
 
-		assertEquals(expectedStrings.toString(), actualStrings.toString());
+		assertEquals("replacements", expectedStrings.toString(),
+				actualStrings.toString());
 	}
 
 	static class Replacement {
--- org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/FilterTestBaseTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/FilterTestBaseTest.java
@@ -16,7 +16,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 import org.jacoco.core.internal.instr.InstrSupport;
+import org.jacoco.core.test.TextBlock;
 import org.junit.ComparisonFailure;
 import org.junit.Test;
 import org.objectweb.asm.Opcodes;
@@ -26,6 +30,73 @@ import org.objectweb.asm.tree.MethodNode;
  * Unit tests for {@link FilterTestBase}.
  */
 public class FilterTestBaseTest extends FilterTestBase {
+
+	@Test
+	public void assertSnapshot_should_throw_when_invalid_comment()
+			throws Exception {
+		try {
+			assertSnapshot(null, context, new StringReader(TextBlock.lines( //
+					"// first comment ignored", //
+					"RETURN", //
+					"// foo")));
+			fail("IllegalStateException expected");
+		} catch (final IllegalStateException e) {
+			assertEquals("Invalid syntax: // foo", e.getMessage());
+		}
+	}
+
+	@Test
+	public void assertSnapshot_should_compare_ranges() throws Exception {
+		final IFilter filter = new IFilter() {
+			public void filter(final MethodNode methodNode,
+					final IFilterContext context, final IFilterOutput output) {
+				output.ignore(methodNode.instructions.getFirst(),
+						methodNode.instructions.getLast());
+			}
+		};
+		try {
+			assertSnapshot(filter, context, new StringReader(TextBlock.lines( //
+					"NOP", //
+					"RETURN", //
+					"// previous instruction starts ignore range NAME", //
+					"// previous instruction ends ignore range NAME")));
+			fail("ComparisonFailure expected");
+		} catch (final ComparisonFailure e) {
+			assertTrue(e.getMessage().startsWith("ignored ranges"));
+			assertEquals("range 0 from instruction 0 to 1\n", e.getActual());
+			assertEquals("range 0 from instruction 1 to 1\n", e.getExpected());
+		}
+	}
+
+	@Test
+	public void assertSnapshot_should_compare_replacements() throws Exception {
+		final IFilter filter = new IFilter() {
+			public void filter(final MethodNode methodNode,
+					final IFilterContext context, final IFilterOutput output) {
+				final Replacements replacements = new Replacements();
+				replacements.add(methodNode.instructions.getFirst(),
+						methodNode.instructions.getFirst(), 0);
+				output.replaceBranches(methodNode.instructions.getFirst(),
+						replacements);
+			}
+		};
+		try {
+			assertSnapshot(filter, context, new StringReader(TextBlock.lines( //
+					"IFEQ LABEL", // ,
+					"// previous instruction replaced by NAME", //
+					"// previous instruction branch 0 creates branch 1 for NAME",
+					"// previous instruction branch 1 creates branch 0 for NAME")));
+			fail("ComparisonFailure expected");
+		} catch (final ComparisonFailure e) {
+			assertTrue(e.getMessage().startsWith("replacements"));
+			assertEquals("[\n" + //
+					"0 if branch 0 of instruction 0]", e.getActual());
+			assertEquals("[\n" + //
+					"0 if branch 1 of instruction 0, \n" + //
+					"1 if branch 0 of instruction 0]", //
+					e.getExpected());
+		}
+	}
 
 	@Test
 	public void assertIgnored_should_throw_ComparisonFailure() {
