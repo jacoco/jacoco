021416720b3360a7b8bae98aea07e362331dcd54
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-05 22:40:22 +0200

    (WIP) take snapshots

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinInlineClassExposeTest.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinInlineClassExposeTest.java
@@ -14,7 +14,7 @@ package org.jacoco.core.test.validation.kotlin;
 
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.kotlin.targets.KotlinInlineClassExposeTarget;
-import org.jacoco.core.test.validation.kotlin.targets.KotlinInlineClassTarget;
+import org.junit.Test;
 
 /**
  * Test of code coverage in {@link KotlinInlineClassExposeTarget}.
@@ -23,6 +23,16 @@ public class KotlinInlineClassExposeTest extends ValidationTestBase {
 
 	public KotlinInlineClassExposeTest() {
 		super(KotlinInlineClassExposeTarget.class);
+	}
+
+	@Test
+	public void bytecodeSnapshots() throws Exception {
+		assertSnapshot(KotlinInlineClassExposeTarget.class, "exposeReturnType",
+				"expose_return_type.txt");
+		assertSnapshot(KotlinInlineClassExposeTarget.class, "exposeParameter",
+				"expose_parameter.txt");
+		assertSnapshot(KotlinInlineClassExposeTarget.class, "exposeUseless",
+				"expose_useless.txt");
 	}
 
 }
