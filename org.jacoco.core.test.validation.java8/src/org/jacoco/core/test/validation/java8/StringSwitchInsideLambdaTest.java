ed75ce3758095553c455cc77415163e30d50eaa5
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-17 09:38:53 +0200

    (WIP) add snapshots

--- org.jacoco.core.test.validation.java8/src/org/jacoco/core/test/validation/java8/StringSwitchInsideLambdaTest.java
+++ org.jacoco.core.test.validation.java8/src/org/jacoco/core/test/validation/java8/StringSwitchInsideLambdaTest.java
@@ -12,8 +12,10 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.java8;
 
+import org.jacoco.core.test.validation.JavaVersion;
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java8.targets.StringSwitchInsideLambdaTarget;
+import org.junit.Test;
 
 /**
  * Test of code coverage in {@link StringSwitchInsideLambdaTarget}.
@@ -22,6 +24,21 @@ public class StringSwitchInsideLambdaTest extends ValidationTestBase {
 
 	public StringSwitchInsideLambdaTest() {
 		super(StringSwitchInsideLambdaTarget.class);
+	}
+
+	@Test
+	public void bytecodeSnapshots() throws Exception {
+		if (!isJDKCompiler) {
+			assertSnapshot(StringSwitchInsideLambdaTarget.class, "lambda$0",
+					"ecj/example.txt");
+		} else {
+			assertSnapshot(StringSwitchInsideLambdaTarget.class,
+					"lambda$example$0",
+					(!JavaVersion.current().isBefore("24")
+							&& JavaVersion.current().isBefore("27") ? "24-26/"
+									: "")
+							+ "example.txt");
+		}
 	}
 
 }
