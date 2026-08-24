5b1f874ca941e2c86432bb42b7d0797686a74ed2
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-24 17:07:56 +0200

    (WIP) add snapshots

--- org.jacoco.core.test.validation.java5/src/org/jacoco/core/test/validation/java5/ControlStructuresTest.java
+++ org.jacoco.core.test.validation.java5/src/org/jacoco/core/test/validation/java5/ControlStructuresTest.java
@@ -14,6 +14,7 @@ package org.jacoco.core.test.validation.java5;
 
 import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.java5.targets.ControlStructuresTarget;
+import org.junit.Test;
 
 /**
  * Tests of basic Java control structures.
@@ -22,6 +23,38 @@ public class ControlStructuresTest extends ValidationTestBase {
 
 	public ControlStructuresTest() {
 		super(ControlStructuresTarget.class);
+	}
+
+	@Test
+	public void bytecodeSnapshots() throws Exception {
+		final String prefix = isJDKCompiler ? "" : "ecj/";
+		assertSnapshot(ControlStructuresTarget.class, "missedIfBlock",
+				prefix + "if.txt");
+		assertSnapshot(ControlStructuresTarget.class, "missedWhileBlock",
+				prefix + "while.txt");
+		assertSnapshot(ControlStructuresTarget.class, "executedDoWhileBlock",
+				"do_while.txt");
+		assertSnapshot(ControlStructuresTarget.class, "missedForBlock",
+				prefix + "for.txt");
+		assertSnapshot(ControlStructuresTarget.class, "missedForEachBlock",
+				prefix + "for_each.txt");
+		assertSnapshot(ControlStructuresTarget.class, "tableSwitchWithHit",
+				"tableswitch.txt");
+		// continuedTableSwitchWithHit
+		// tableSwitchWithoutHit
+		// lookupSwitchWithHit
+		assertSnapshot(ControlStructuresTarget.class, "lookupSwitchWithHit",
+				"lookupswitch.txt");
+		// continuedLookupSwitchWithHit
+		// lookupSwitchWithoutHit
+		// switchImplicitDefaultNotExecuted
+		assertSnapshot(ControlStructuresTarget.class, "breakStatement",
+				prefix + "break.txt");
+		assertSnapshot(ControlStructuresTarget.class, "continueStatement",
+				prefix + "continue.txt");
+		// conditionalReturn
+		// implicitReturn
+		// explicitReturn
 	}
 
 }
