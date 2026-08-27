28c2e514f4f1b408022eb427171de2894781330e
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2026-08-27 16:59:23 +0200

    Use `KotlinValidationTestBase` in `KotlinUnsafeCastOperatorTest` (#2266)

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinUnsafeCastOperatorTest.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinUnsafeCastOperatorTest.java
@@ -12,40 +12,24 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.kotlin;
 
-import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.kotlin.targets.KotlinUnsafeCastOperatorTarget;
 import org.junit.Test;
-
-import kotlin.KotlinVersion;
 
 /**
  * Test of "unsafe" cast operator.
  */
-public class KotlinUnsafeCastOperatorTest extends ValidationTestBase {
+public class KotlinUnsafeCastOperatorTest extends KotlinValidationTestBase {
 
 	public KotlinUnsafeCastOperatorTest() {
 		super(KotlinUnsafeCastOperatorTarget.class);
 	}
 
+	/** Starting from {@link #KOTLIN_1_3} */
 	@Test
 	public void bytecodeSnapshots() throws Exception {
-		if (KotlinVersion.CURRENT.isAtLeast(1, 7)) {
-			assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
-					"unsafe_cast_operator.txt");
-		} else if (KotlinVersion.CURRENT.isAtLeast(1, 6)) {
-			// https://github.com/JetBrains/kotlin/commit/041773fd2584bc279813361eb7fc11ae84c214fd
-			assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
-					"1.6/unsafe_cast_operator.txt");
-		} else if (KotlinVersion.CURRENT.isAtLeast(1, 5)) {
-			assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
-					"1.5/unsafe_cast_operator.txt");
-		} else if (KotlinVersion.CURRENT.isAtLeast(1, 4)) {
-			assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
-					"1.4/unsafe_cast_operator.txt");
-		} else {
-			assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
-					"1.3/unsafe_cast_operator.txt");
-		}
+		assertSnapshot(KotlinUnsafeCastOperatorTarget.class, "example",
+				"unsafe_cast_operator.txt", //
+				KOTLIN_1_7, KOTLIN_1_6, KOTLIN_1_5, KOTLIN_1_4, KOTLIN_1_3);
 	}
 
 }
