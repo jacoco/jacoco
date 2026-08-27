cfda843c0524649f2fd4bb33598f5ba19347c5de
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-27 10:59:51 +0200

    Use `KotlinValidationTestBase` in `KotlinUnsafeCastOperatorTest`

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinValidationTestBase.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinValidationTestBase.java
@@ -20,6 +20,11 @@ import kotlin.KotlinVersion;
 
 public abstract class KotlinValidationTestBase extends ValidationTestBase {
 
+	protected static final KotlinVersion KOTLIN_1_3 = new KotlinVersion(1, 3);
+	protected static final KotlinVersion KOTLIN_1_4 = new KotlinVersion(1, 4);
+	protected static final KotlinVersion KOTLIN_1_5 = new KotlinVersion(1, 5);
+	protected static final KotlinVersion KOTLIN_1_6 = new KotlinVersion(1, 6);
+	protected static final KotlinVersion KOTLIN_1_7 = new KotlinVersion(1, 7);
 	protected static final KotlinVersion KOTLIN_2_1 = new KotlinVersion(2, 1);
 	protected static final KotlinVersion KOTLIN_2_2 = new KotlinVersion(2, 2);
 	protected static final KotlinVersion KOTLIN_2_4 = new KotlinVersion(2, 4);
