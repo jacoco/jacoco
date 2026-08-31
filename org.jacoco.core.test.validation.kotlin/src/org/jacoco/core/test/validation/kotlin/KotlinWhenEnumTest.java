bff739ea6ea7db139fc072c3ac45289937bb625c
Evgeny Mandrikov <mandrikov@gmail.com>
2026-08-31 14:29:47 +0200

    (WIP) snapshots

--- org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinWhenEnumTest.java
+++ org.jacoco.core.test.validation.kotlin/src/org/jacoco/core/test/validation/kotlin/KotlinWhenEnumTest.java
@@ -12,34 +12,39 @@
  *******************************************************************************/
 package org.jacoco.core.test.validation.kotlin;
 
-import org.jacoco.core.test.validation.ValidationTestBase;
 import org.jacoco.core.test.validation.kotlin.targets.KotlinWhenEnumTarget;
 import org.junit.Test;
 
 /**
  * Test of code coverage in {@link KotlinWhenEnumTarget}.
  */
-public class KotlinWhenEnumTest extends ValidationTestBase {
+public class KotlinWhenEnumTest extends KotlinValidationTestBase {
 
 	public KotlinWhenEnumTest() {
 		super(KotlinWhenEnumTarget.class);
 	}
 
+	/** Starting from {@link #KOTLIN_1_6} */
 	@Test
 	public void bytecodeSnapshots() throws Exception {
 		assertSnapshot(KotlinWhenEnumTarget.class, "whenEnum",
-				"without_else.txt");
+				"without_else.txt", //
+				KOTLIN_2_0, KOTLIN_1_6);
 		assertSnapshot(KotlinWhenEnumTarget.class, "whenEnumRedundantElse",
-				"redundant_else.txt");
+				"redundant_else.txt", //
+				KOTLIN_1_6);
 		assertSnapshot(KotlinWhenEnumTarget.class,
 				"whenByNullableEnumWithNullCaseAndWithoutElse",
-				"nullable_case_without_else.txt");
+				"nullable_case_without_else.txt", //
+				KOTLIN_2_0, KOTLIN_1_6);
 		assertSnapshot(KotlinWhenEnumTarget.class,
 				"whenByNullableEnumWithoutNullCaseAndWithElse",
-				"nullable_else.txt");
+				"nullable_else.txt", //
+				KOTLIN_1_6);
 		assertSnapshot(KotlinWhenEnumTarget.class,
 				"whenByNullableEnumWithNullAndElseCases",
-				"nullable_case_with_else.txt");
+				"nullable_case_with_else.txt", //
+				KOTLIN_1_6);
 	}
 
 }
